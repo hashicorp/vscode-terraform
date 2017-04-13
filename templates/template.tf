8439da1409c3acbc83212ad2a7e4acf0e2755151
Mikael Olenfalk <olenfalk@amazon.com>
2017-04-13 12:54:53 +0200

    Convert tmLanguage file to json file for easier editing
    Also syntax highlight string_interpolations in heredocs

--- templates/template.tf
+++ templates/template.tf
@@ -7,11 +7,25 @@ variable "region" {
 }
 
 resource "aws_s3_bucket" "agent_bucket" {
-  bucket = "${var.stage}-${var.region}-mybucket"
-  acl    = "private"
+  bucket   = "${var.stage}-${var.region}-mybucket"
+  acl      = "private"
+  property = "value"
+
+  smurf = <<HEREDOC
+Lorem ${var.ipsum} dolor sit amet, consectetur adipiscing elit,
+sed do eiusmod tempor incididunt ut labore et dolore magna
+aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+ullamco laboris nisi ut aliquip ex ea commodo consequat.
+Duis aute irure ${lookup(var.ipsum, "dolor")} in reprehenderit
+in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+HEREDOC
 
   logging {
     target_bucket = "${var.stage}-${var.region}-mymonitoringbucket"
     target_prefix = "aprefix/"
+  }
+
+  subkey {
+    smurf = "gargamel"
   }
 }
