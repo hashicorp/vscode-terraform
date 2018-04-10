ee46f7e2aebf42f96e20bd9ee1ef4d5ccb919c12
Jonathan Carter <joncart@outlook.com>
2018-04-10 10:00:05 -0700

    Restricting language services to local files

--- src/extension.ts
+++ src/extension.ts
@@ -9,6 +9,11 @@ import { CompletionProvider } from './providers';
 export let errorDiagnosticCollection = vscode.languages.createDiagnosticCollection("terraform-error");
 export let outputChannel = vscode.window.createOutputChannel("Terraform");
 
+const documentSelector: vscode.DocumentSelector = [
+    { language: "terraform", scheme: "file" },
+    { language: "terraform", scheme: "untitled" }
+];
+
 export function activate(ctx: vscode.ExtensionContext) {
     ctx.subscriptions.push(errorDiagnosticCollection);
 
@@ -16,15 +21,17 @@ export function activate(ctx: vscode.ExtensionContext) {
 
     // we need to manually handle save events otherwise format on autosave does not work
     ctx.subscriptions.push(vscode.workspace.onDidSaveTextDocument((doc) => formattingProvider.onSave(doc)));
-    vscode.languages.registerDocumentFormattingEditProvider('terraform', formattingProvider);
+    vscode.languages.registerDocumentFormattingEditProvider(documentSelector, formattingProvider);
 
     ctx.subscriptions.push(vscode.workspace.onDidChangeTextDocument(liveIndex));
 
     ctx.subscriptions.push(vscode.commands.registerCommand('terraform.validate', () => { validateCommand(); }));
     ctx.subscriptions.push(vscode.commands.registerCommand('terraform.lint', () => { lintCommand(); }));
 
-    ctx.subscriptions.push(vscode.languages.registerCompletionItemProvider("terraform", new CompletionProvider, '.', '"'));
+    ctx.subscriptions.push(vscode.languages.registerCompletionItemProvider(documentSelector, new CompletionProvider, '.', '"'));
 
     // index operations
-    initializeIndex(ctx);
+    if (vscode.workspace.rootPath) {
+        initializeIndex(ctx);
+    }
 }
