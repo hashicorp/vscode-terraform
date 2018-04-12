a1c472fe422d807bd143b1840793260b0cf83e43
Jonathan Carter <joncart@outlook.com>
2018-04-12 09:19:09 -0700

    Prevent document save/change events for guests

--- src/extension.ts
+++ src/extension.ts
@@ -18,12 +18,7 @@ export function activate(ctx: vscode.ExtensionContext) {
     ctx.subscriptions.push(errorDiagnosticCollection);
 
     let formattingProvider = new FormattingEditProvider;
-
-    // we need to manually handle save events otherwise format on autosave does not work
-    ctx.subscriptions.push(vscode.workspace.onDidSaveTextDocument((doc) => formattingProvider.onSave(doc)));
     vscode.languages.registerDocumentFormattingEditProvider(documentSelector, formattingProvider);
-
-    ctx.subscriptions.push(vscode.workspace.onDidChangeTextDocument(liveIndex));
 
     ctx.subscriptions.push(vscode.commands.registerCommand('terraform.validate', () => { validateCommand(); }));
     ctx.subscriptions.push(vscode.commands.registerCommand('terraform.lint', () => { lintCommand(); }));
@@ -32,6 +27,10 @@ export function activate(ctx: vscode.ExtensionContext) {
 
     // index operations
     if (vscode.workspace.rootPath) {
+        // we need to manually handle save events otherwise format on autosave does not work
+        ctx.subscriptions.push(vscode.workspace.onDidSaveTextDocument((doc) => formattingProvider.onSave(doc)));
+        ctx.subscriptions.push(vscode.workspace.onDidChangeTextDocument(liveIndex));
+
         initializeIndex(ctx);
     }
 }
