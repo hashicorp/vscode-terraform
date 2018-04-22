783059a42c63008909f7f3a1776746c0ca849035
Mikael Olenfalk <mikael@olenfalk.se>
2018-04-22 11:26:09 +0200

    Correctly throw exception on parse error

--- src/index/hcl-hil.ts
+++ src/index/hcl-hil.ts
@@ -4,11 +4,27 @@ export interface Ast {
   Node: any;
 }
 
+export class ParseError extends Error {
+  readonly fileName: string;
+  readonly offset: number;
+  readonly column: number;
+  readonly line: number;
+
+  constructor(node: any, message?: string) {
+    super(message);
+
+    this.fileName = node.Pos.Filename;
+    this.offset = node.Pos.Offset;
+    this.line = node.Pos.Line - 1;
+    this.column = node.Pos.Column - 1;
+  }
+}
+
 export function parseHcl(document: string): Ast {
   let result = hcl.parseHcl(document);
 
   if (result[1]) {
-    throw new Error("parse error");
+    throw new ParseError(result[1]);
   }
 
   return result[0] as Ast;
