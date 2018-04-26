5622dd202670ac1cd9a4ea9fcc43f0c4627c13ff
Mikael Olenfalk <mikael@olenfalk.se>
2018-04-26 22:24:23 +0200

    Properly emit the error message

--- hcl-hil/main.go
+++ hcl-hil/main.go
@@ -3,20 +3,65 @@ package main
 import (
 	"github.com/gopherjs/gopherjs/js"
 	"github.com/hashicorp/hcl"
+	hclParser "github.com/hashicorp/hcl/hcl/parser"
+	hclToken "github.com/hashicorp/hcl/hcl/token"
 	"github.com/hashicorp/hil"
 	"github.com/hashicorp/hil/ast"
+	"github.com/hashicorp/hil/parser"
 )
 
-func parseHcl(v string) (interface{}, error) {
-	return hcl.ParseString(v)
+type hclError struct {
+	Pos *hclToken.Pos
+	Err string
 }
 
-func parseHilWithPosition(v string, column, line int, filename string) (interface{}, error) {
-	return hil.ParseWithPosition(v, ast.Pos{
+func parseHcl(v string) (interface{}, *hclError) {
+	result, err := hcl.ParseString(v)
+
+	if err != nil {
+		if pErr, ok := err.(*hclParser.PosError); ok {
+			return nil, &hclError{
+				Pos: &pErr.Pos,
+				Err: pErr.Err.Error(),
+			}
+		}
+
+		return result, &hclError{
+			Pos: nil,
+			Err: err.Error(),
+		}
+	}
+
+	return result, nil
+}
+
+type hilError struct {
+	Pos *ast.Pos
+	Err string
+}
+
+func parseHilWithPosition(v string, column, line int, filename string) (interface{}, *hilError) {
+	result, err := hil.ParseWithPosition(v, ast.Pos{
 		Column:   column,
 		Line:     line,
 		Filename: filename,
 	})
+
+	if err != nil {
+		if pErr, ok := err.(*parser.ParseError); ok {
+			return nil, &hilError{
+				Pos: &pErr.Pos,
+				Err: pErr.String(),
+			}
+		}
+
+		return nil, &hilError{
+			Pos: nil,
+			Err: err.Error(),
+		}
+	}
+
+	return result, nil
 }
 
 func main() {
