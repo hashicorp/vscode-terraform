e15ee82c3c108dbe061a0ed531186846498117b2
Mikael Olenfalk <mikael@olenfalk.se>
2018-05-05 01:27:55 +0200

    Add simple async readFile helper

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,5 +1,18 @@
 import * as vscode from 'vscode';
+import { readFile } from 'fs';
 
 export function isTerraformDocument(document: vscode.TextDocument): boolean {
   return document.languageId === "terraform";
+}
+
+export function read(path: string): Promise<string> {
+  return new Promise<string>((resolve, reject) => {
+    readFile(path, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data.toString('utf8'));
+      }
+    });
+  });
 }
