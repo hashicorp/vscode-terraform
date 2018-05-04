c67e708fdf7225845fee8f9ed82a114d428c8e91
Mikael Olenfalk <mikael@olenfalk.se>
2018-05-05 01:28:24 +0200

    Make runner strip ansi by default

--- src/runner.ts
+++ src/runner.ts
@@ -1,7 +1,21 @@
+import * as vscode from 'vscode';
+
 import { getConfiguration } from "./configuration";
 import { execFile } from "child_process";
+import { stripAnsi } from "./ansi";
 
-export function runTerraform(args: string[], input?: string): Promise<string> {
+export interface TerraformInvocationOptions {
+  input?: string;
+  keepAnsi?: boolean;
+}
+
+function processOutput(output: string, options: TerraformInvocationOptions): string {
+  if (options.keepAnsi)
+    return output;
+  return stripAnsi(output);
+}
+
+export function runTerraform(args: string[], options: TerraformInvocationOptions = {}): Promise<string> {
   let path = getConfiguration().path;
   console.log(`Running terraform cwd='${process.cwd()}' path='${path}' args=[${args.join(", ")}]`);
 
@@ -10,17 +24,18 @@ export function runTerraform(args: string[], input?: string): Promise<string> {
     const child = execFile(path, args, {
       encoding: 'utf8',
       maxBuffer: 1024 * 1024,
+      cwd: vscode.workspace.workspaceFolders[0].uri.fsPath
     }, (error, stdout, stderr) => {
       if (error) {
         console.log(`Running terraform failed: ${error}: ${stderr}`);
-        reject(stderr);
+        reject(processOutput(stderr, options));
       } else {
-        resolve(stdout);
+        resolve(processOutput(stdout, options));
       }
     });
 
-    if (input) {
-      child.stdin.write(input);
+    if (options.input) {
+      child.stdin.write(options.input);
       child.stdin.end();
     }
   });
