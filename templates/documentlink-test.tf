111b23de74cfbbd00ce38e7b536e67088877da15
Mikael Olenfalk <mikael@olenfalk.se>
2018-05-06 23:58:07 +0200

    Use untitled files for documentlink tests

--- templates/documentlink-test.tf
+++
@@ -1,3 +0,0 @@
-resource "aws_s3_bucket" "document-link-test" {
-  bucket = "document-link-test"
-}
--- test/documentlink.test.ts
+++ test/documentlink.test.ts
@@ -4,7 +4,12 @@ import { uriFromRelativePath } from '../src/helpers';
 
 suite("DocumentLink Tests", () => {
     test("Links resource sections", async () => {
-        let doc = await vscode.workspace.openTextDocument(uriFromRelativePath("documentlink-test.tf"));
+        let doc = await vscode.workspace.openTextDocument({
+            language: "terraform",
+            content: 'resource "aws_s3_bucket" "document-link-test" {\n' +
+                     '  bucket = "document-link-test"\n' +
+                     '}'
+        });
 
         let successful = await vscode.commands.executeCommand('terraform.index-document', doc.uri) as boolean;
         assert(successful, "forced indexing not successful");
