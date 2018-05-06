02ba854a730b47dd5d0c7b9097204f218a214424
Mikael Olenfalk <mikael@olenfalk.se>
2018-05-06 22:50:27 +0200

    Add two simple string helpers

--- src/helpers.ts
+++ src/helpers.ts
@@ -26,3 +26,27 @@ export function uriFromRelativePath(path: string, folder?: vscode.WorkspaceFolde
     path: [folder.uri.path, path].join('/')
   });
 }
+
+type MatcherFunction = (character: string) => boolean;
+
+export function backwardsSearch(haystack: string, matcher: MatcherFunction): number {
+  if (haystack.length === 0)
+    return -1;
+
+  for (let i = haystack.length - 1; i >= 0; i--) {
+    if (matcher(haystack[i])) {
+      return i;
+    }
+  }
+
+  return -1;
+}
+
+export function count(haystack: string, character: string): number {
+  let result = 0;
+  for (let i = 0; i < haystack.length; i++) {
+    if (haystack[i] === character)
+      result++;
+  }
+  return result;
+}
--- test/helpers.test.ts
+++ test/helpers.test.ts
@@ -13,4 +13,15 @@ suite("Helper Tests", () => {
             done();
         });
     });
+
+    test("backwardsSearch returns -1 if not found", () => {
+        assert.equal(helpers.backwardsSearch("haystack", () => false), -1);
+    });
+
+    test("backwardsSearch returns index", () => {
+        let index = helpers.backwardsSearch("haystack", () => true);
+        assert.equal(index, 7);
+        assert.equal("haystack"[index], "k");
+    });
+
 });
