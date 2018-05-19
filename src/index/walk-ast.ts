473da052f58ae7ddd2f3a463574ab402f5121266
Mikael Olenfalk <mikael@olenfalk.se>
2018-05-19 20:47:07 +0200

    Support walking a string rather than file in walk-ast.ts

--- src/index/walk-ast.ts
+++ src/index/walk-ast.ts
@@ -7,9 +7,14 @@ let hcl = require('../hcl-hil.js');
 if (process.argv.length < 2) {
   console.log("ERROR: Missing argument.");
 } else {
-  let path = process.argv[2];
+  let text = "";
+  if (process.argv.length === 2) {
+    let path = process.argv[2];
 
-  let text = fs.readFileSync(path).toString();
+    text = fs.readFileSync(path).toString();
+  } else {
+    text = process.argv[3];
+  }
   let [ast, error] = hcl.parseHcl(text);
   if (error) {
     console.log("ERROR:", error);
