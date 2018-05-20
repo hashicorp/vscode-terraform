2fddad00231f82e92a6dccf284da9f257ac84bb3
Mikael Olenfalk <mikael@olenfalk.se>
2018-05-20 21:15:56 +0200

    activate: Use IndexLocator instead of Index

--- src/extension.ts
+++ src/extension.ts
@@ -16,6 +16,7 @@ import { liveIndex } from './live';
 import { RenameProvider } from './rename';
 import * as telemetry from './telemetry';
 import { validateCommand } from './validate';
+import { IndexLocator } from './index/index-locator';
 
 export let ErrorDiagnosticCollection = vscode.languages.createDiagnosticCollection("terraform-error");
 export let outputChannel = vscode.window.createOutputChannel("Terraform");
@@ -25,8 +26,10 @@ const documentSelector: vscode.DocumentSelector = [
     { language: "terraform", scheme: "untitled" }
 ];
 
+export let indexLocator: IndexLocator;
+
 export function activate(ctx: vscode.ExtensionContext) {
-    let index = new Index();
+    indexLocator = new IndexLocator(ctx);
 
     telemetry.activate(ctx);
 
@@ -47,12 +50,14 @@ export function activate(ctx: vscode.ExtensionContext) {
         vscode.commands.registerCommand('terraform.validate', () => { validateCommand(); }),
         vscode.commands.registerCommand('terraform.lint', () => { lintCommand(); }),
         vscode.commands.registerCommand('terraform.showReferences', (section: Section) => {
-            showReferencesCommand(index, section);
+            showReferencesCommand(indexLocator.getIndexForSection(section), section);
         }),
         vscode.commands.registerCommand('terraform.reindex', () => {
-            index.clear();
+            for (let index of indexLocator.allIndices(false)) {
+                index.clear();
+            }
             if (getConfiguration().indexing.enabled) {
-                initialCrawl(index);
+                initialCrawl(indexLocator);
             }
         }),
         vscode.commands.registerCommand('terraform.index-document', (uri: vscode.Uri): boolean => {
@@ -62,12 +67,19 @@ export function activate(ctx: vscode.ExtensionContext) {
                 return false;
             }
 
+            let index = indexLocator.getIndexForUri(uri);
             return !!index.indexDocument(doc);
         }),
         vscode.commands.registerCommand('terraform.preview-graph', () => {
-            graphCommand(index, graphProvider);
+            graphCommand(indexLocator, graphProvider);
         }),
-        vscode.commands.registerCommand('terraform.navigate-to-section', async (args: { targetId: string }) => {
+        vscode.commands.registerCommand('terraform.navigate-to-section', async (args: { workspaceFolderName: string, targetId: string }) => {
+            let folder = vscode.workspace.workspaceFolders.find((f) => f.name === args.workspaceFolderName);
+            if (!folder) {
+                await vscode.window.showErrorMessage(`Cannot find workspace folder with name ${args.workspaceFolderName}`);
+                return;
+            }
+            let index = indexLocator.getIndexForWorkspaceFolder(folder);
             let section = index.section(args.targetId);
             if (!section) {
                 await vscode.window.showErrorMessage(`No section with id ${args.targetId}`);
@@ -78,30 +90,30 @@ export function activate(ctx: vscode.ExtensionContext) {
         }),
 
         // providers
-        vscode.languages.registerCompletionItemProvider(documentSelector, new CompletionProvider(index), '.', '"', '{', '(', '['),
-        vscode.languages.registerDefinitionProvider(documentSelector, new DefinitionProvider(index)),
-        vscode.languages.registerDocumentSymbolProvider(documentSelector, new DocumentSymbolProvider(index)),
-        vscode.languages.registerWorkspaceSymbolProvider(new WorkspaceSymbolProvider(index)),
-        vscode.languages.registerReferenceProvider(documentSelector, new ReferenceProvider(index)),
-        vscode.languages.registerRenameProvider(documentSelector, new RenameProvider(index)),
-        vscode.languages.registerHoverProvider(documentSelector, new HoverProvider(index)),
-        vscode.languages.registerDocumentLinkProvider(documentSelector, new DocumentLinkProvider(index))
+        vscode.languages.registerCompletionItemProvider(documentSelector, new CompletionProvider(indexLocator), '.', '"', '{', '(', '['),
+        vscode.languages.registerDefinitionProvider(documentSelector, new DefinitionProvider(indexLocator)),
+        vscode.languages.registerDocumentSymbolProvider(documentSelector, new DocumentSymbolProvider(indexLocator)),
+        vscode.languages.registerWorkspaceSymbolProvider(new WorkspaceSymbolProvider(indexLocator)),
+        vscode.languages.registerReferenceProvider(documentSelector, new ReferenceProvider(indexLocator)),
+        vscode.languages.registerRenameProvider(documentSelector, new RenameProvider(indexLocator)),
+        vscode.languages.registerHoverProvider(documentSelector, new HoverProvider(indexLocator)),
+        vscode.languages.registerDocumentLinkProvider(documentSelector, new DocumentLinkProvider(indexLocator))
     );
 
     if (getConfiguration().codelens.enabled) {
-        ctx.subscriptions.push(vscode.languages.registerCodeLensProvider(documentSelector, new CodeLensProvider(index)));
+        ctx.subscriptions.push(vscode.languages.registerCodeLensProvider(documentSelector, new CodeLensProvider(indexLocator)));
     }
 
     // operations which should only work in a local context (as opposed to live-share)
     if (vscode.workspace.rootPath) {
         // we need to manually handle save events otherwise format on autosave does not work
         ctx.subscriptions.push(vscode.workspace.onDidSaveTextDocument((doc) => formattingProvider.onSave(doc)));
-        ctx.subscriptions.push(vscode.workspace.onDidChangeTextDocument((e) => liveIndex(index, e)));
+        ctx.subscriptions.push(vscode.workspace.onDidChangeTextDocument((e) => liveIndex(indexLocator, e)));
 
         // start to build the index
         if (getConfiguration().indexing.enabled) {
-            ctx.subscriptions.push(createWorkspaceWatcher(index));
-            initialCrawl(index);
+            ctx.subscriptions.push(createWorkspaceWatcher(indexLocator));
+            initialCrawl(indexLocator);
         }
     }
 
