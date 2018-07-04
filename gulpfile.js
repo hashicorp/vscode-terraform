e283a41432a48c9d9f33921c6780c18e747bb122
Mikael Olenfalk <mikael@olenfalk.se>
2018-07-05 00:55:14 +0200

    Mark releases as draft and ensure vsix is attached

--- gulpfile.js
+++ gulpfile.js
@@ -177,8 +177,41 @@ gulp.task('test-no-fail', () => {
         });
 });
 
+// release notes
+gulp.task('generate-release-notes', function generateReleaseNotes(done) {
+    fs.readFile('./CHANGELOG.md', (err, data) => {
+        if (err) {
+            log.error(`${chalk.red('ERROR')}: could not read CHANGELOG.md: ${err}`);
+            done(err);
+        }
+
+        const regex = new RegExp(/^# ([0-9]).([0-9]).([0-9]).*/);
+        const lines = data.toString().split('\n');
+
+        const firstHeaderLine = lines.findIndex((l) => l.match(regex));
+        if (firstHeaderLine === -1) {
+            log.error(`${chalk.red('ERROR')}: could not find first header`);
+            done(new Error("could not find first header"));
+        }
+
+        const secondHeaderLine = lines.findIndex((l, index) => {
+            return firstHeaderLine < index && l.match(regex);
+        });
+
+        fs.writeFile("out/LAST_RELEASE.md", lines.slice(firstHeaderLine, secondHeaderLine).join("\n"), (err) => {
+            if (err) {
+                log.error(`${chalk.red('ERROR')}: could not write out/LAST_RELEASE.md: ${err}`);
+                done(err);
+            } else {
+                done();
+            }
+        });
+    })
+});
+
 // compile
-gulp.task('build', gulp.series('generate-hcl-hil.js', 'copy-autocompletion-data', 'copy-html-templates', 'generate-constants-keyfile', 'compile'));
+gulp.task('build', gulp.series('generate-hcl-hil.js', 'copy-autocompletion-data', 'copy-html-templates',
+                                    'generate-constants-keyfile', 'generate-release-notes', 'compile'));
 
 // watch
 gulp.task('watch', gulp.series('build', () => {
