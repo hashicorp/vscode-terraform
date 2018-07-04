9974fdd80899e8e52d7bdf9bf8f88c7e4d4cf175
Mikael Olenfalk <mikael@olenfalk.se>
2018-07-04 23:25:44 +0200

    Emit telemetry in symbol providers

--- src/index/providers.ts
+++ src/index/providers.ts
@@ -1,5 +1,7 @@
 import * as vscode from 'vscode';
+import { Reporter } from '../telemetry';
 import { IndexLocator } from './index-locator';
+import { Section } from './section';
 import { Uri } from './uri';
 import { from_vscode_Position } from './vscode-adapter';
 
@@ -20,30 +22,39 @@ export class ReferenceProvider implements vscode.ReferenceProvider {
   }
 }
 
+function getSymbolInfo(s: Section): vscode.SymbolInformation {
+  const location = new vscode.Location(vscode.Uri.parse(s.location.uri.toString()),
+  new vscode.Range(s.location.range.start.line, s.location.range.start.character,
+    s.location.range.end.line, s.location.range.end.character));
+  const containerName = [s.sectionType, s.type].filter((f) => !!f).join(".");
+  return new vscode.SymbolInformation(s.name, getKind(s.sectionType), containerName, location);
+}
+
+function getKind(sectionType: string): vscode.SymbolKind {
+  switch (sectionType) {
+    case "resource": return vscode.SymbolKind.Interface;
+    case "output": return vscode.SymbolKind.Property;
+    case "variable": return vscode.SymbolKind.Variable;
+    case "local": return vscode.SymbolKind.Variable;
+  }
+
+  return null;
+}
+
 export class DocumentSymbolProvider implements vscode.DocumentSymbolProvider {
   constructor(private indexLocator: IndexLocator) { }
 
   provideDocumentSymbols(document: vscode.TextDocument): vscode.SymbolInformation[] {
-    const sections = this.indexLocator.getIndexForDoc(document).query(Uri.parse(document.uri.toString()));
+    try {
+      const sections = this.indexLocator.getIndexForDoc(document).query(Uri.parse(document.uri.toString()));
+      const symbols = sections.map((s) => getSymbolInfo(s));
 
-    return sections.map((s) => {
-      const location = new vscode.Location(vscode.Uri.parse(s.location.uri.toString()),
-        new vscode.Range(s.location.range.start.line, s.location.range.start.character,
-          s.location.range.end.line, s.location.range.end.character));
-      const containerName = [s.sectionType, s.type].filter((f) => !!f).join(".");
-      return new vscode.SymbolInformation(s.name, this.getKind(s.sectionType), containerName, location);
-    });
-  }
-
-  private getKind(sectionType: string): vscode.SymbolKind {
-    switch (sectionType) {
-      case "resource": return vscode.SymbolKind.Interface;
-      case "output": return vscode.SymbolKind.Property;
-      case "variable": return vscode.SymbolKind.Variable;
-      case "local": return vscode.SymbolKind.Variable;
+      Reporter.trackEvent("provideDocumentSymbols", {}, { symbolCount: symbols.length });
+      return symbols;
+    } catch (err) {
+      Reporter.trackException("provideDocumentSymbols", err);
+      throw err;
     }
-
-    return null;
   }
 }
 
@@ -51,8 +62,17 @@ export class WorkspaceSymbolProvider implements vscode.WorkspaceSymbolProvider {
   constructor(private indexLocator: IndexLocator) { }
 
   provideWorkspaceSymbols(query: string): vscode.SymbolInformation[] {
-    let indices = [...this.indexLocator.allIndices(true)];
+    try {
+      let indices = [...this.indexLocator.allIndices(true)];
 
-    return [].concat(...indices.map((i) => i.query("ALL_FILES", { name: query })));
+      const sections = Array<Section>().concat(...indices.map((i) => i.query("ALL_FILES", { name: query })));
+      const symbols = sections.map((s) => getSymbolInfo(s));
+
+      Reporter.trackEvent("provideWorkspaceSymbols", {}, { symbolCount: symbols.length });
+      return symbols;
+    } catch (err) {
+      Reporter.trackException("provideWorkspaceSymbols", err);
+      throw err;
+    }
   }
 }
