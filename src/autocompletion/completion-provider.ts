511d4bad7f095358781c5258ddabda9794532b98
Mikael Olenfalk <mikael@olenfalk.se>
2018-07-17 20:54:51 +0200

    Catch exceptions during completion providing

--- src/autocompletion/completion-provider.ts
+++ src/autocompletion/completion-provider.ts
@@ -8,6 +8,8 @@ import { Section } from "../index/section";
 import { GetKnownFunctions, InterpolationFunctionDefinition } from './builtin-functions';
 import { allProviders, IFieldDef, terraformConfigAutoComplete } from './model';
 import { SectionCompletions } from './section-completions';
+import { Logger } from "../logger";
+import { Reporter } from "../telemetry";
 
 const resourceExp = new RegExp("(resource|data)\\s+(\")?(\\w+)(\")?\\s+(\")?([\\w\\-]+)(\")?\\s+({)");
 const terraformExp = new RegExp("(variable|output|module)\\s+(\")?([\\w\\-]+)(\")?\\s+({)");
@@ -15,6 +17,8 @@ const nestedRegexes: RegExp[] = [/\w[A-Za-z0-9\-_]*(\s*){/, /\w[A-Za-z0-9\-_]*(\
 const propertyExp = new RegExp("^([\\w_-]+)$");
 
 export class CompletionProvider implements vscode.CompletionItemProvider {
+  private logger = new Logger("completion-provider");
+
   constructor(private indexLocator: IndexLocator) { }
 
   private sectionToCompletionItem(section: Section, needInterpolation: boolean, range?: vscode.Range): vscode.CompletionItem {
@@ -103,95 +107,100 @@ export class CompletionProvider implements vscode.CompletionItemProvider {
   }
 
   provideCompletionItems(document: vscode.TextDocument, position: vscode.Position): vscode.CompletionItem[] {
-    // figure out if we are inside a string
-    const [ast, error] = parseHcl(document.getText());
-    if (ast) {
-      let offset1 = document.offsetAt(position);
-      let pos: AstPosition = {
-        Line: position.line + 1,
-        Column: position.character + 1,
-        Offset: 0,
-        Filename: ''
-      };
-      let token = getTokenAtPosition(ast, pos);
-      if (token) {
-        let interpolationCompletions = this.interpolationCompletions(document, position);
-        if (interpolationCompletions.length !== 0)
-          return interpolationCompletions;
-      }
-    }
-
-    // TODO: refactor to use ast here aswell
-    let lineText = document.lineAt(position.line).text;
-    let lineTillCurrentPosition = lineText.substr(0, position.character);
-
-    // high-level types ex: variable, resource, module, output etc..
-    if (position.character === 0 || this.isTerraformTypes(lineText)) {
-      return this.getFilteredTerraformTypes(lineText);
-    }
-
-    // resource | data "resource type auto completion"
-    let possibleResources: any[] = this.lookupForTerraformResource(lineTillCurrentPosition);
-    if (possibleResources.length > 0) {
-      return this.getAutoCompletion(possibleResources, vscode.CompletionItemKind.Class);
-    }
-
-    // auto-completion for property types including 1 level deeep nested types
-    if (lineTillCurrentPosition.trim().length === 0 || propertyExp.test(lineTillCurrentPosition.trim())) {
-      let prev: number = position.line - 1;
-      let nestedTypes: string[] = [];
-      let parentResource: string = "";
-      let parentType: string = "";
-      let nestedCounts: number = 0;
-      while (prev >= 0) {
-        let line: string = document.lineAt(prev).text;
-        // nested closing block
-        if (line.trim() === "}") {
-          nestedCounts++;
+    try {
+      // figure out if we are inside a string
+      const [ast, error] = parseHcl(document.getText());
+      if (ast) {
+        let offset1 = document.offsetAt(position);
+        let pos: AstPosition = {
+          Line: position.line + 1,
+          Column: position.character + 1,
+          Offset: 0,
+          Filename: ''
+        };
+        let token = getTokenAtPosition(ast, pos);
+        if (token) {
+          let interpolationCompletions = this.interpolationCompletions(document, position);
+          if (interpolationCompletions.length !== 0)
+            return interpolationCompletions;
         }
-        // for now only 1-level deep
-        if (this.isNestedLevelType(line) && nestedCounts === 0) {
-          nestedTypes.push(this.getTypeFromLine(line, 0));
-        }
-        if (this.typedMatched(line, terraformExp)) {
-          parentType = this.getTypeFromLine(line, 0);
-          break;
-        }
-        if (this.typedMatched(line, resourceExp)) {
-          parentType = this.getTypeFromLine(line, 0);
-          parentResource = this.getTypeFromLine(line, 1);
-          break;
-        }
-        prev--;
       }
 
-      if (nestedTypes.length > 0 && parentResource.length > 0) {
-        let temp: any = { items: allProviders[parentType][parentResource].args };
-        let fieldArgs: IFieldDef[] = _.cloneDeep(temp).items;
-        if (parentType === "resource") {
-          fieldArgs.push(...terraformConfigAutoComplete.resource);
-        }
-        fieldArgs.push(...allProviders[parentType][parentResource].args);
-        let argumentsLength: number = nestedTypes.length - 1;
-        let lastArgName: string = "";
-        while (argumentsLength >= 0) {
-          const field: IFieldDef = fieldArgs.find((arg) => arg.name === nestedTypes[argumentsLength]);
-          fieldArgs = field.args;
-          lastArgName = field.name;
-          argumentsLength--;
-        }
-        return this.getItemsForArgs(fieldArgs, lastArgName);
-      } else if (parentResource.length > 0) {
-        let temp: any = { items: allProviders[parentType][parentResource].args };
-        let fieldArgs: IFieldDef[] = _.cloneDeep(temp).items;
-        if (parentType === "resource") {
-          fieldArgs.push(...terraformConfigAutoComplete.resource);
-        }
-        return this.getItemsForArgs(fieldArgs, parentResource);
-      } else if (parentType.length > 0 && nestedTypes.length === 0) {
-        let fieldArgs: IFieldDef[] = terraformConfigAutoComplete[parentType];
-        return this.getItemsForArgs(fieldArgs, parentType);
+      // TODO: refactor to use ast here aswell
+      let lineText = document.lineAt(position.line).text;
+      let lineTillCurrentPosition = lineText.substr(0, position.character);
+
+      // high-level types ex: variable, resource, module, output etc..
+      if (position.character === 0 || this.isTerraformTypes(lineText)) {
+        return this.getFilteredTerraformTypes(lineText);
       }
+
+      // resource | data "resource type auto completion"
+      let possibleResources: any[] = this.lookupForTerraformResource(lineTillCurrentPosition);
+      if (possibleResources.length > 0) {
+        return this.getAutoCompletion(possibleResources, vscode.CompletionItemKind.Class);
+      }
+
+      // auto-completion for property types including 1 level deeep nested types
+      if (lineTillCurrentPosition.trim().length === 0 || propertyExp.test(lineTillCurrentPosition.trim())) {
+        let prev: number = position.line - 1;
+        let nestedTypes: string[] = [];
+        let parentResource: string = "";
+        let parentType: string = "";
+        let nestedCounts: number = 0;
+        while (prev >= 0) {
+          let line: string = document.lineAt(prev).text;
+          // nested closing block
+          if (line.trim() === "}") {
+            nestedCounts++;
+          }
+          // for now only 1-level deep
+          if (this.isNestedLevelType(line) && nestedCounts === 0) {
+            nestedTypes.push(this.getTypeFromLine(line, 0));
+          }
+          if (this.typedMatched(line, terraformExp)) {
+            parentType = this.getTypeFromLine(line, 0);
+            break;
+          }
+          if (this.typedMatched(line, resourceExp)) {
+            parentType = this.getTypeFromLine(line, 0);
+            parentResource = this.getTypeFromLine(line, 1);
+            break;
+          }
+          prev--;
+        }
+
+        if (nestedTypes.length > 0 && parentResource.length > 0) {
+          let temp: any = { items: allProviders[parentType][parentResource].args };
+          let fieldArgs: IFieldDef[] = _.cloneDeep(temp).items;
+          if (parentType === "resource") {
+            fieldArgs.push(...terraformConfigAutoComplete.resource);
+          }
+          fieldArgs.push(...allProviders[parentType][parentResource].args);
+          let argumentsLength: number = nestedTypes.length - 1;
+          let lastArgName: string = "";
+          while (argumentsLength >= 0) {
+            const field: IFieldDef = fieldArgs.find((arg) => arg.name === nestedTypes[argumentsLength]);
+            fieldArgs = field.args;
+            lastArgName = field.name;
+            argumentsLength--;
+          }
+          return this.getItemsForArgs(fieldArgs, lastArgName);
+        } else if (parentResource.length > 0) {
+          let temp: any = { items: allProviders[parentType][parentResource].args };
+          let fieldArgs: IFieldDef[] = _.cloneDeep(temp).items;
+          if (parentType === "resource") {
+            fieldArgs.push(...terraformConfigAutoComplete.resource);
+          }
+          return this.getItemsForArgs(fieldArgs, parentResource);
+        } else if (parentType.length > 0 && nestedTypes.length === 0) {
+          let fieldArgs: IFieldDef[] = terraformConfigAutoComplete[parentType];
+          return this.getItemsForArgs(fieldArgs, parentType);
+        }
+      }
+    } catch (error) {
+      this.logger.exception("Failed to provide completions", error);
+      Reporter.trackException("provideCompletionItems", error);
     }
     return [];
   }
