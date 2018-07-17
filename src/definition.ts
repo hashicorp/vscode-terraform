4fed6709789c5cff9c2165cb24d7a597d5a90910
Mikael Olenfalk <mikael@olenfalk.se>
2018-07-17 20:57:06 +0200

    Catch exceptions during definition providing

--- src/definition.ts
+++ src/definition.ts
@@ -1,18 +1,27 @@
 import * as vscode from 'vscode';
 import { IndexLocator } from './index/index-locator';
 import { from_vscode_Position, from_vscode_Uri, to_vscode_Location } from './index/vscode-adapter';
+import { Logger } from './logger';
+import { Reporter } from './telemetry';
 
 export class DefinitionProvider implements vscode.DefinitionProvider {
+  private logger = new Logger("definition-provider");
+
   constructor(private indexLocator: IndexLocator) { }
 
   provideDefinition(document: vscode.TextDocument, position: vscode.Position): vscode.Location {
-    let reference = this.indexLocator.getIndexForDoc(document).queryReferences(from_vscode_Uri(document.uri), { position: from_vscode_Position(position) })[0];
-    if (!reference)
-      return null;
+    try {
+      let reference = this.indexLocator.getIndexForDoc(document).queryReferences(from_vscode_Uri(document.uri), { position: from_vscode_Position(position) })[0];
+      if (!reference)
+        return null;
 
-    let section = this.indexLocator.getIndexForDoc(document).query("ALL_FILES", reference.getQuery())[0];
-    if (!section)
-      return null;
-    return to_vscode_Location(section.location);
+      let section = this.indexLocator.getIndexForDoc(document).query("ALL_FILES", reference.getQuery())[0];
+      if (!section)
+        return null;
+      return to_vscode_Location(section.location);
+    } catch (error) {
+      this.logger.exception("Could not provide definition", error);
+      Reporter.trackException("provideDefinition", error);
+    }
   }
 }
