98f57e93774cb1aac07af7ca5d916402e258ce4a
Mikael Olenfalk <mikael@olenfalk.se>
2018-07-17 21:09:34 +0200

    Catch exceptions during link providing

--- src/documentlink.ts
+++ src/documentlink.ts
@@ -3,23 +3,32 @@ import { findResourceFormat } from './autocompletion/model';
 import { getConfiguration } from './configuration';
 import { IndexLocator } from './index/index-locator';
 import { to_vscode_Range } from './index/vscode-adapter';
+import { Logger } from './logger';
+import { Reporter } from './telemetry';
 
 export class DocumentLinkProvider implements vscode.DocumentLinkProvider {
-    constructor(private indexLocator: IndexLocator) { }
+  private logger = new Logger("document-link-provider");
 
-    provideDocumentLinks(document: vscode.TextDocument, token: vscode.CancellationToken): vscode.ProviderResult<vscode.DocumentLink[]> {
-        let index = this.indexLocator.getIndexForDoc(document).getOrIndexDocument(document, { exclude: getConfiguration().indexing.exclude });
-        if (!index)
-            return [];
-        return index.sections.map((s) => {
-            if (!s.type)
-                return null;
+  constructor(private indexLocator: IndexLocator) { }
 
-            let doc = findResourceFormat(s.sectionType, s.type);
-            if (!doc)
-                return null;
+  provideDocumentLinks(document: vscode.TextDocument, token: vscode.CancellationToken): vscode.ProviderResult<vscode.DocumentLink[]> {
+    try {
+      let index = this.indexLocator.getIndexForDoc(document).getOrIndexDocument(document, { exclude: getConfiguration().indexing.exclude });
+      if (!index)
+        return [];
 
-            return new vscode.DocumentLink(to_vscode_Range(s.typeLocation.range), vscode.Uri.parse(doc.url));
-        }).filter((d) => d != null);
+      return index.sections.map((s) => {
+        if (!s.type)
+          return null;
+        let doc = findResourceFormat(s.sectionType, s.type);
+        if (!doc)
+          return null;
+
+        return new vscode.DocumentLink(to_vscode_Range(s.typeLocation.range), vscode.Uri.parse(doc.url));
+      }).filter((d) => d != null);
+    } catch (error) {
+      this.logger.exception("Could not provide document links", error);
+      Reporter.trackException("provideDocumentLinks", error);
     }
-};
+  }
+}
