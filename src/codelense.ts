7b328b08c94afa3b9108ad1b95f84fc743e1ac10
Mikael Olenfalk <mikael@olenfalk.se>
2018-07-17 21:13:05 +0200

    Catch exceptions during code lense providing

--- src/codelense.ts
+++ src/codelense.ts
@@ -3,6 +3,8 @@ import { getConfiguration } from './configuration';
 import { Index } from './index';
 import { IndexLocator } from './index/index-locator';
 import { Section } from './index/section';
+import { Logger } from './logger';
+import { Reporter } from './telemetry';
 
 export interface TerraformCodeLens {
   type: "REFERENCE" | "PLAN";
@@ -32,6 +34,8 @@ export class SectionReferenceCodeLens extends vscode.CodeLens implements Terrafo
 }
 
 export class CodeLensProvider implements vscode.CodeLensProvider {
+  private logger = new Logger("code-lens-provider");
+
   private eventEmitter = new vscode.EventEmitter<void>();
   onDidChangeCodeLenses = this.eventEmitter.event;
 
@@ -42,19 +46,30 @@ export class CodeLensProvider implements vscode.CodeLensProvider {
   }
 
   provideCodeLenses(document: vscode.TextDocument, token: vscode.CancellationToken): vscode.ProviderResult<vscode.CodeLens[]> {
-    let index = this.indexLocator.getIndexForDoc(document);
-    let fileIndex = index.getOrIndexDocument(document, { exclude: getConfiguration().indexing.exclude });
-    if (!fileIndex)
+    try {
+      let index = this.indexLocator.getIndexForDoc(document);
+      let fileIndex = index.getOrIndexDocument(document, { exclude: getConfiguration().indexing.exclude });
+      if (!fileIndex)
+        return [];
+      return fileIndex.sections.filter((s) => s.sectionType !== "provider").map((s) => {
+        let firstLineOfSection = document.lineAt(s.location.range.start.line).range;
+        return new SectionReferenceCodeLens(index, firstLineOfSection, s);
+      });
+    } catch (error) {
+      this.logger.exception("Could not provide code lenses", error);
+      Reporter.trackException("provideCodeLenses", error);
       return [];
-    return fileIndex.sections.filter((s) => s.sectionType !== "provider").map((s) => {
-      let firstLineOfSection = document.lineAt(s.location.range.start.line).range;
-      return new SectionReferenceCodeLens(index, firstLineOfSection, s);
-    });
+    }
   }
 
   resolveCodeLens?(codeLens: vscode.CodeLens, token: vscode.CancellationToken): vscode.ProviderResult<vscode.CodeLens> {
-    let sectionReferenceCodeLens = codeLens as SectionReferenceCodeLens;
-    sectionReferenceCodeLens.command = sectionReferenceCodeLens.createCommand();
-    return sectionReferenceCodeLens;
+    try {
+      let sectionReferenceCodeLens = codeLens as SectionReferenceCodeLens;
+      sectionReferenceCodeLens.command = sectionReferenceCodeLens.createCommand();
+      return sectionReferenceCodeLens;
+    } catch (error) {
+      this.logger.exception("Could not resolve code lens", error);
+      Reporter.trackException("resolveCodeLens", error);
+    }
   }
 }
