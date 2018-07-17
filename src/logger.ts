fdecfb1c74ae2f354a448494eff941a27ae4e8b0
Mikael Olenfalk <mikael@olenfalk.se>
2018-07-17 19:33:46 +0200

    Add Logger

---
+++ src/logger.ts
@@ -0,0 +1,103 @@
+
+export interface Output {
+  appendLine(line: string);
+  show(preserveFocus: boolean);
+}
+
+export enum Level {
+  Error,
+  Warn,
+  Info,
+  Debug,
+  Trace
+}
+
+export function configure(output: Output) {
+  LoggingOutput.output = output;
+}
+
+export class Logger {
+  constructor(readonly prefix: string) { }
+
+  public exception(message: string, error: Error, ...params: any[]) {
+    LoggingOutput.log(Level.Error, this.prefix, message, error, ...params);
+  }
+
+  public error(message: string, ...params: any[]) {
+    LoggingOutput.log(Level.Error, this.prefix, message, ...params);
+  }
+
+  public warn(message: string, ...params: any[]) {
+    LoggingOutput.log(Level.Warn, this.prefix, message, ...params);
+  }
+
+  public info(message: string, ...params: any[]) {
+    LoggingOutput.log(Level.Info, this.prefix, message, ...params);
+  }
+
+  public debug(message: string, ...params: any[]) {
+    LoggingOutput.log(Level.Debug, this.prefix, message, ...params);
+  }
+
+  public trace(message: string, ...params: any[]) {
+    LoggingOutput.log(Level.Trace, this.prefix, message, ...params);
+  }
+}
+
+const isDebuggingRegex = /^--inspect(-brk)?=?/;
+
+class LoggingOutput {
+  public static output: Output | undefined;
+  private static _isDebugging: boolean | undefined;
+
+  static log(level: Level, prefix: string, message: string, ...params: any[]) {
+    const formattedPrefix = this.formatPrefix(level, prefix);
+    if (this.isDebugging) {
+      console.log(formattedPrefix, message, ...params);
+    }
+
+    if (!this.output) {
+      return;
+    }
+
+    this.output.appendLine([formattedPrefix, message, ...params].join(" "));
+    if (level === Level.Error || level === Level.Warn) {
+      this.output.show(true);
+    }
+  }
+
+  private static formatPrefix(level: Level, prefix: string) {
+    function lvlStr(l: Level) {
+      switch (l) {
+        case Level.Error: return "ERROR";
+        case Level.Warn: return "WARN ";
+        case Level.Info: return "INFO ";
+        case Level.Debug: return "DEBUG";
+        case Level.Trace: return "TRACE";
+      }
+    }
+
+    return `${this.timestamp} [${lvlStr(level)}] ${prefix}:`;
+  }
+
+  private static get timestamp(): string {
+    const now = new Date();
+    const time = now
+      .toISOString()
+      .replace(/T/, ' ')
+      .replace(/\..+/, '');
+    return `${time}:${('00' + now.getUTCMilliseconds()).slice(-3)}`;
+  }
+
+  private static get isDebugging() {
+    if (this._isDebugging === undefined) {
+      try {
+        const args = process.execArgv;
+
+        this._isDebugging = args ? args.some(arg => isDebuggingRegex.test(arg)) : false;
+      } catch { }
+    }
+
+    return this._isDebugging;
+  }
+}
