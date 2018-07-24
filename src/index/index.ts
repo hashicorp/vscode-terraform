223272146af23b64e6661ae73e47b9e2fcd66bce
Mikael Olenfalk <mikael@olenfalk.se>
2018-07-24 20:16:14 +0200

    Add clear function on Index

--- src/index/index.ts
+++ src/index/index.ts
@@ -1,5 +1,6 @@
 import { FileIndex } from "./file-index";
 import { IndexGroup } from "./group";
+import { Section } from "./section";
 import { Uri } from "./uri";
 
 
@@ -20,6 +21,11 @@ export class Index {
         return [...this._groups.values()];
     }
 
+    clear() {
+        this._groups.forEach(g => g.clear());
+        this._groups.clear();
+    }
+
     /// Returns the group with the specified Uri
     group(uri: Uri | string): IndexGroup {
         const s = uri.toString();
@@ -27,8 +33,11 @@ export class Index {
     }
 
     /// Returns the group to which the specified uri would belong
-    groupFor(uri: Uri | FileIndex): IndexGroup {
-        return this.groups.find(g => g.belongs(uri));
+    groupFor(uriOrFileIndexOrSection: Uri | FileIndex | Section): IndexGroup {
+        if (uriOrFileIndexOrSection instanceof Section)
+            return this.groups.find(g => g.belongs(uriOrFileIndexOrSection.location.uri));
+        else
+            return this.groups.find(g => g.belongs(uriOrFileIndexOrSection));
     }
 
     add(file: FileIndex): IndexGroup {
