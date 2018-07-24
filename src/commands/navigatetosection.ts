e01336d5ce982fe827f5052cf7ab21125b0fa3c1
Mikael Olenfalk <mikael@olenfalk.se>
2018-07-24 20:15:07 +0200

    Adapt navigate to section command after indexing refactoring

--- src/commands/navigatetosection.ts
+++ src/commands/navigatetosection.ts
@@ -1,25 +1,29 @@
 import * as vscode from "vscode";
-import { indexLocator } from "../extension";
+import { IndexAdapter } from "../index/index-adapter";
 import { to_vscode_Range, to_vscode_Uri } from "../index/vscode-adapter";
 import { Command } from "./command";
 
 export class NavigateToSectionCommand extends Command {
-  constructor() {
+  constructor(private index: IndexAdapter) {
     super("navigate-to-section");
   }
 
-  protected async perform(args: { workspaceFolderName: string, targetId: string }): Promise<any> {
-    let folder = vscode.workspace.workspaceFolders.find((f) => f.name === args.workspaceFolderName);
-    if (!folder) {
-      return await vscode.window.showErrorMessage(`Cannot find workspace folder with name ${args.workspaceFolderName}`);
+  protected async perform(args: { groupUri: string, targetId: string }): Promise<any> {
+    const group = this.index.index.group(args.groupUri);
+    if (!group) {
+      throw new Error(`Cannot find index group with uri ${args.groupUri}`);
     }
 
-    let index = indexLocator.getIndexForWorkspaceFolder(folder);
-    let section = index.section(args.targetId);
+    const section = group.section(args.targetId);
     if (!section) {
       return await vscode.window.showErrorMessage(`No section with id ${args.targetId}`);
     }
 
-    await vscode.window.showTextDocument(to_vscode_Uri(section.location.uri), { selection: to_vscode_Range(section.location.range) });
+    await vscode.window.showTextDocument(
+      to_vscode_Uri(section.location.uri),
+      {
+        selection: to_vscode_Range(section.location.range)
+      }
+    );
   }
 }
