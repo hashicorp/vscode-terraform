595d9b8155a878516c22b2943ca56f3e873fe4b8
Mikael Olenfalk <mikael@olenfalk.se>
2018-07-26 06:28:36 +0200

    Use Runner when calling terraform validate

--- src/commands/validate.ts
+++ src/commands/validate.ts
@@ -1,11 +1,11 @@
-import { execFile } from 'child_process';
 import * as vscode from 'vscode';
 import { getConfiguration } from '../configuration';
 import { IndexAdapter } from '../index/index-adapter';
+import { Runner } from '../runner';
 import { Command } from './command';
 
 export class ValidateCommand extends Command {
-  constructor(private index: IndexAdapter) {
+  constructor(private index: IndexAdapter, private runner: Runner) {
     super("validate");
   }
 
@@ -15,7 +15,7 @@ export class ValidateCommand extends Command {
     try {
       for (const group of this.index.index.groups) {
         this.logger.info(`Validating group ${group.uri.toString()}`);
-        const output = await validate(path, group.uri.toString());
+        const output = await this.runner.run({}, "validate", "-no-color", group.uri.toString());
         for (const line of output.split('\n')) {
           this.logger.info("output: ", line);
         }
@@ -25,24 +25,4 @@ export class ValidateCommand extends Command {
       return await vscode.window.showErrorMessage("Validation failed, more information in the output tab.");
     }
   }
-}
-
-function validate(execPath: string, directory: string): Promise<string> {
-  return new Promise<string>((resolve, reject) => {
-    let commandLineArgs = ["validate", "-no-color"];
-    if (directory !== undefined) {
-      commandLineArgs.push(directory);
-    }
-
-    const child = execFile(execPath, commandLineArgs, {
-      encoding: 'utf8',
-      maxBuffer: 1024 * 1024
-    }, (error, stdout, stderr) => {
-      if (error) {
-        reject(stderr);
-      } else {
-        resolve(stdout);
-      }
-    });
-  });
 }
