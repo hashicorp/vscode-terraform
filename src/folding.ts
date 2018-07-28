d7f8c077fc2cb11265c8c0183fc68994069720cc
Mikael Olenfalk <mikael@olenfalk.se>
2018-07-28 02:00:36 +0200

    Add logging and error telemetry to folding provider

--- src/folding.ts
+++ src/folding.ts
@@ -3,44 +3,52 @@ import { IndexAdapter } from './index/index-adapter';
 import { Location } from './index/location';
 import { Property } from './index/property';
 import { Section } from './index/section';
+import { Logger } from './logger';
+import { Reporter } from './telemetry';
 
 export class CodeFoldingProvider implements vscode.FoldingRangeProvider {
   private startMarker = new RegExp("^\\s*#region");
   private endMarker = new RegExp("^\\s*#endregion");
+  private logger = new Logger("folding-provider");
 
   constructor(private index: IndexAdapter) { }
 
   async provideFoldingRanges(document: vscode.TextDocument): Promise<vscode.FoldingRange[]> {
-    let [file, group] = await this.index.indexDocument(document);
-    if (!file)
-      return undefined;
+    try {
+      let [file, group] = await this.index.indexDocument(document);
+      if (!file)
+        return undefined;
 
-    let ranges: vscode.FoldingRange[] = [];
-    for (const section of file.sections) {
-      ranges.push(this.sectionToRange(section));
+      let ranges: vscode.FoldingRange[] = [];
+      for (const section of file.sections) {
+        ranges.push(this.sectionToRange(section));
 
-      for (const property of section.properties) {
-        const range = this.propertyToRange(property);
-        if (range)
-          ranges.push(range);
-      }
-    }
-
-    // handle #region #endregion markers
-    let regionStart = -1;
-    for (let line = 0; line < document.lineCount; line++) {
-      const text = document.lineAt(line);
-      if (regionStart === -1) {
-        if (text.text.match(this.startMarker))
-          regionStart = line;
-      } else {
-        if (text.text.match(this.endMarker)) {
-          ranges.push(new vscode.FoldingRange(regionStart, line, vscode.FoldingRangeKind.Comment));
-          regionStart = -1;
+        for (const property of section.properties) {
+          const range = this.propertyToRange(property);
+          if (range)
+            ranges.push(range);
         }
       }
+
+      // handle #region #endregion markers
+      let regionStart = -1;
+      for (let line = 0; line < document.lineCount; line++) {
+        const text = document.lineAt(line);
+        if (regionStart === -1) {
+          if (text.text.match(this.startMarker))
+            regionStart = line;
+        } else {
+          if (text.text.match(this.endMarker)) {
+            ranges.push(new vscode.FoldingRange(regionStart, line, vscode.FoldingRangeKind.Comment));
+            regionStart = -1;
+          }
+        }
+      }
+      return ranges;
+    } catch (err) {
+      this.logger.exception("Could not provide folding ranges", err);
+      Reporter.trackException("provideFoldingRanges", err);
     }
-    return ranges;
   }
 
   private locationToLines(location: Location): [number, number] {
