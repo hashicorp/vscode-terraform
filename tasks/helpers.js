1006446836e97da1fe885b391e0c0bfabb3332c0
Mikael Olenfalk <mikael@olenfalk.se>
2018-09-17 09:13:39 +0200

    Move appinsights build flag to helpers
    (Just like the other args)

--- tasks/helpers.js
+++ tasks/helpers.js
@@ -1,5 +1,6 @@
 
 module.exports = {
   offlineBuild: process.argv.indexOf("--offline-build") !== -1,
-  forceWrapperGeneration: process.argv.indexOf("--force-wrapper-generation") !== -1
+  forceWrapperGeneration: process.argv.indexOf("--force-wrapper-generation") !== -1,
+  requireAppInsightsKey: process.argv.indexOf("--require-appinsights-key") !== -1
 };
