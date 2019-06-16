7527f2313a89b8f23d197682807f181240a470a0
kterada0509 <hiramatsu_kentaro@cyberagent.co.jp>
2019-06-17 00:49:16 +0900

    Add support datadog provider autocompletion

--- test/completion-provider.test.ts
+++ test/completion-provider.test.ts
@@ -212,6 +212,7 @@ suite("Autocompletion Tests", () => {
             assert(!!shouldHaveCompletion(completions, "google_project_iam_member", vscode.CompletionItemKind.Class), "should some google completion");
             assert(!!shouldHaveCompletion(completions, "oci_audit_configuration", vscode.CompletionItemKind.Class), "should have some oci completion");
             assert(!!shouldHaveCompletion(completions, "openstack_compute_keypair_v2", vscode.CompletionItemKind.Class), "should have some openstack completion");
+            assert(!!shouldHaveCompletion(completions, "datadog_user", vscode.CompletionItemKind.Class), "should have some datadog completion");
         });
 
         test("Complete properties", async () => {
