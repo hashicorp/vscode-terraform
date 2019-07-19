4721957f48cefae13a48389fedb49c468e68d15c
Mikael Olenfalk <mikael@olenfalk.se>
2019-07-19 20:00:36 +0200

    Make integration tests work again

--- test/run-test.ts
+++ test/run-test.ts
@@ -3,16 +3,18 @@ import { runTests } from 'vscode-test';
 
 async function main() {
   try {
-    // The folder containing the Extension Manifest package.json
-    // Passed to `--extensionDevelopmentPath`
     const extensionDevelopmentPath = path.resolve(__dirname, '../../');
 
-    // The path to the extension test runner script
-    // Passed to --extensionTestsPath
     const extensionTestsPath = path.resolve(__dirname, './index');
 
+    const testWorkspacePath = path.resolve(__dirname, "../../templates");
+
     // Download VS Code, unzip it and run the integration test
-    await runTests({ extensionDevelopmentPath, extensionTestsPath });
+    await runTests({
+      extensionDevelopmentPath,
+      extensionTestsPath,
+      launchArgs: [testWorkspacePath]
+    });
   } catch (err) {
     console.error('Failed to run tests');
     process.exit(1);
