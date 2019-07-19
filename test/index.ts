3c785f78bc6a9c6d788aa15c511b0d37d1752cd0
Mikael Olenfalk <mikael@olenfalk.se>
2019-07-19 19:34:21 +0200

    Migrate to vscode-test for integration tests

--- test/index.ts
+++ test/index.ts
@@ -1,9 +1,37 @@
-import * as testRunner from 'vscode/lib/testrunner';
+import * as glob from 'glob';
+import * as Mocha from 'mocha';
+import * as path from 'path';
 
-testRunner.configure({
-    ui: 'tdd',
-    useColors: true,
-    forbidOnly: process.env.CI ? true : false
-});
+export function run(): Promise<void> {
+    // Create the mocha test
+    const mocha = new Mocha({
+        ui: 'tdd',
+    });
+    mocha.useColors(true);
 
-module.exports = testRunner;
+    const testsRoot = path.resolve(__dirname, '..');
+
+    return new Promise((c, e) => {
+        glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
+            if (err) {
+                return e(err);
+            }
+
+            // Add files to the test suite
+            files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
+
+            try {
+                // Run the mocha test
+                mocha.run(failures => {
+                    if (failures > 0) {
+                        e(new Error(`${failures} tests failed.`));
+                    } else {
+                        c();
+                    }
+                });
+            } catch (err) {
+                e(err);
+            }
+        });
+    });
+}
