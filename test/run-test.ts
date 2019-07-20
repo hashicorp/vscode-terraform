5ca5c1f7dbb6a1bf85a3aa6f61352af49d3c784a
Mikael Olenfalk <mikael@olenfalk.se>
2019-07-20 16:01:39 +0200

    Add more logging when running tests

--- test/run-test.ts
+++ test/run-test.ts
@@ -22,7 +22,7 @@ async function main() {
     // Download VS Code, unzip it and run the integration test
     await runTests(options);
   } catch (err) {
-    console.error('Failed to run tests');
+    console.error('Failed to run tests', err);
     process.exit(1);
   }
 }
