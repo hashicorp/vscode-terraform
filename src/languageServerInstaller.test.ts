5167a9f3ae9644e2457a82a21514c37843f97574
Alex Pilon <apilon@hashicorp.com>
2020-09-03 21:24:44 -0400

    update ref to file

--- src/languageServerInstaller.test.ts
+++ src/languageServerInstaller.test.ts
@@ -7,7 +7,7 @@ suite('LS installer', () => {
 	test('should calculate correct file sha256 sum', async () => {
 		const installer = new LanguageServerInstaller;
 		const expectedSum = "0314c6a66b059bde92c5ed0f11601c144cbd916eff6d1241b5b44e076e5888dc";
-		const testPath = path.resolve(__dirname, "..", "testFixture", "shasum.txt");
+		const testPath = path.resolve(__dirname, "..", "testFixture", "shasumtest.txt");
 
 		const sum = await installer.calculateFileSha256Sum(testPath);
 		assert.strictEqual(sum, expectedSum);
