92e65304f20c270f681dc0a206f007750fecbd04
Audrey Eschright <audrey@hashicorp.com>
2020-10-22 16:16:43 -0700

    Disable workspace folder test per VSCode limitation

--- src/test/workspaces.test.ts
+++ src/test/workspaces.test.ts
@@ -1,22 +1,24 @@
-import * as assert from 'assert';
-import * as vscode from 'vscode';
-import { getDocUri, open } from './helper';
+// Disabling test due to VSCode limitations - "Entering a new workspace is not possible in tests"
 
-suite('workspace folders', () => {
-	const docUri = getDocUri('sample.tf');
-	const folder = getDocUri('modules/');
+// import * as assert from 'assert';
+// import * as vscode from 'vscode';
+// import { getDocUri, open } from './helper';
 
-	test('should only have one client for nested workspace folders', async () => {
-		await addNestedWorkspaces(docUri, folder);
-	});
-});
+// suite('workspace folders', () => {
+// 	const docUri = getDocUri('sample.tf');
+// 	const folder = getDocUri('modules/');
 
-async function addNestedWorkspaces(docUri: vscode.Uri, folderUri: vscode.Uri) {
-	const ext = vscode.extensions.getExtension('hashicorp.terraform');
-	await open(docUri);
+// 	test('should only have one client for nested workspace folders', async () => {
+// 		await addNestedWorkspaces(docUri, folder);
+// 	});
+// });
 
-	assert.ok(vscode.workspace.updateWorkspaceFolders(1, null, { uri: folderUri }));
-	const clients = ext.exports.clients;
-	assert.strictEqual(clients.size, 1);
-	assert.strictEqual(vscode.workspace.workspaceFolders.length, 2);
-}
+// async function addNestedWorkspaces(docUri: vscode.Uri, folderUri: vscode.Uri) {
+// 	const ext = vscode.extensions.getExtension('hashicorp.terraform');
+// 	await open(docUri);
+
+// 	assert.ok(vscode.workspace.updateWorkspaceFolders(1, null, { uri: folderUri }));
+// 	const clients = ext.exports.clients;
+// 	assert.strictEqual(clients.size, 1);
+// 	assert.strictEqual(vscode.workspace.workspaceFolders.length, 2);
+// }
