459798cfdf9f56b4b1c9ae0988827c689df774a0
Audrey Eschright <audrey@hashicorp.com>
2020-12-15 13:04:24 -0800

    Remove ubuntu again

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -26,6 +26,8 @@ async function main(): Promise<void> {
     // Download VS Code, unzip it and run the integration test
     // start in the fixtures folder to prevent the language server from walking all the
     // project root folders, like node_modules
+    await runTests({ extensionDevelopmentPath, extensionTestsPath, launchArgs: ['testFixture'] }); // use current release
+
     await runTests({
       extensionDevelopmentPath,
       extensionTestsPath,
@@ -39,8 +41,6 @@ async function main(): Promise<void> {
       version: '1.48.2',
       launchArgs: ['testFixture']
     });
-
-    await runTests({ extensionDevelopmentPath, extensionTestsPath, launchArgs: ['testFixture'] });
   } catch (err) {
     console.error(err);
     console.error('Failed to run tests');
