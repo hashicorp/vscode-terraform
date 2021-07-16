d65731a6ca34dcb7ab23d69b1f845e3d730034aa
Radek Simko <radek.simko@gmail.com>
2021-07-16 17:24:44 +0100

    utils: Remove unused function

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,4 @@
 import * as cp from 'child_process';
-import * as https from 'https';
 
 export function exec(cmd: string, args: readonly string[]): Promise<{ stdout: string, stderr: string }> {
 	return new Promise((resolve, reject) => {
@@ -9,25 +8,6 @@ export function exec(cmd: string, args: readonly string[]): Promise<{ stdout: st
 			}
 			return resolve({ stdout, stderr });
 		});
-	});
-}
-
-export function httpsRequest(url: string, options: https.RequestOptions = {}, encoding = 'utf8'): Promise<string> {
-	return new Promise((resolve, reject) => {
-		https.request(url, options, res => {
-			if (res.statusCode === 301 || res.statusCode === 302) { // follow redirects
-				return resolve(httpsRequest(res.headers.location, options, encoding));
-			}
-			if (res.statusCode !== 200) {
-				return reject(res.statusMessage);
-			}
-			let body = '';
-			res.setEncoding(encoding)
-				.on('data', data => body += data)
-				.on('end', () => resolve(body));
-		})
-			.on('error', reject)
-			.end();
 	});
 }
 
