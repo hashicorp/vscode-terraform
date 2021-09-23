f656f1cb1c8046350a08510936428e4368809b8a
James Pogran <jpogran@outlook.com>
2021-09-23 10:25:53 -0400

    Update minimum VS Code engine to 1.52.0 (#780)
    This updates the vscode-languageclient library to the 7.0.0 release, which requires the minimum vs code engine to be 1.52.0.
    
    The vscode-languageclient 7.0.0 release is the current supported release, but we pinned to 7.0.0-next.12. We shouldn't be running on the interim releases, but we were waiting on https://github.com/hashicorp/terraform-ls/pull/608 to merge to support the changed jsonrpc constructs in 7.0.0. Now that that is merged, released, and in the wild for a significant amount of time we can move to the 7.0.0 vscode-languageclient version.
    
    This requires us to increment the minimum supported version of VS Code to 1.52.0. This means any user with a VS Code version lower than 1.52.0 will not see an updated extension in the marketplace, but will still receive updated terraform-ls releases through the automatic updater.

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -26,20 +26,24 @@ async function main(): Promise<void> {
     // Download VS Code, unzip it and run the integration test
     // start in the fixtures folder to prevent the language server from walking all the
     // project root folders, like node_modules
-    await runTests({ extensionDevelopmentPath, extensionTestsPath, launchArgs: ['testFixture', '--disable-extensions'] }); // use current release
+    await runTests({
+      extensionDevelopmentPath,
+      extensionTestsPath,
+      launchArgs: ['testFixture', '--disable-extensions'],
+    }); // use current release
 
     await runTests({
       extensionDevelopmentPath,
       extensionTestsPath,
       version: 'insiders',
-      launchArgs: ['testFixture', '--disable-extensions']
+      launchArgs: ['testFixture', '--disable-extensions'],
     });
 
     await runTests({
       extensionDevelopmentPath,
       extensionTestsPath,
-      version: '1.50.1',
-      launchArgs: ['testFixture', '--disable-extensions']
+      version: '1.52.0',
+      launchArgs: ['testFixture', '--disable-extensions'],
     });
   } catch (err) {
     console.error(err);
