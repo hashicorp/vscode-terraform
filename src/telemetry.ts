cdaff10d2e35baae36c1dc51f38b8fff6e3df3bb
Radek Simko <radek.simko@gmail.com>
2021-11-23 18:26:40 +0100

    Process telemetry from language server (#823)
    * Process telemetry from language server
    
    * avoid unnecessary class property
    
    * avoid an implicit any type

---
+++ src/telemetry.ts
@@ -0,0 +1,38 @@
+import TelemetryReporter from 'vscode-extension-telemetry';
+import { BaseLanguageClient, ClientCapabilities, StaticFeature } from 'vscode-languageclient';
+
+const TELEMETRY_VERSION = 1;
+
+type TelemetryEvent = {
+  v: number;
+  name: string;
+  properties: { [key: string]: unknown };
+};
+
+export class TelemetryFeature implements StaticFeature {
+  constructor(private client: BaseLanguageClient, reporter: TelemetryReporter) {
+    this.client.onTelemetry((event: TelemetryEvent) => {
+      if (event.v != TELEMETRY_VERSION) {
+        console.log(`unsupported telemetry event: ${event}`);
+        return;
+      }
+
+      reporter.sendRawTelemetryEvent(event.name, event.properties);
+    });
+  }
+
+  public fillClientCapabilities(capabilities: ClientCapabilities): void {
+    if (!capabilities['experimental']) {
+      capabilities['experimental'] = {};
+    }
+    capabilities['experimental']['telemetryVersion'] = TELEMETRY_VERSION;
+  }
+
+  public initialize(): void {
+    return;
+  }
+
+  public dispose(): void {
+    return;
+  }
+}
