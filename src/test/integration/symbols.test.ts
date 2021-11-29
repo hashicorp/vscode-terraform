26dd4403d5728741b7d0488a6a71efc3b050bb48
Daniel Banck <daniel@dbanck.de>
2021-11-29 16:08:09 +0100

    format codebase with new prettier version

--- src/test/integration/symbols.test.ts
+++ src/test/integration/symbols.test.ts
@@ -8,27 +8,26 @@ suite('document symbols', () => {
     return await vscode.commands.executeCommand('workbench.action.closeActiveEditor');
   });
 
-	const docUri = getDocUri('sample.tf');
+  const docUri = getDocUri('sample.tf');
 
-	test('returns symbols', async () => {
-		await testSymbols(docUri, [
-			'provider "vault"',
-			'resource "vault_auth_backend" "b"',
-			'module "local"'
-		]);
-	});
+  test('returns symbols', async () => {
+    await testSymbols(docUri, ['provider "vault"', 'resource "vault_auth_backend" "b"', 'module "local"']);
+  });
 });
 
 async function testSymbols(docUri: vscode.Uri, symbolNames: string[]) {
-	await open(docUri);
-	// Executing the command `vscode.executeDocumentSymbolProvider` to simulate requesting doc symbols
-	const symbols = (await vscode.commands.executeCommand('vscode.executeDocumentSymbolProvider', docUri)) as vscode.SymbolInformation[];
+  await open(docUri);
+  // Executing the command `vscode.executeDocumentSymbolProvider` to simulate requesting doc symbols
+  const symbols = (await vscode.commands.executeCommand(
+    'vscode.executeDocumentSymbolProvider',
+    docUri,
+  )) as vscode.SymbolInformation[];
 
-	assert.ok(symbols);
-	expect(symbols).not.to.be.undefined;
+  assert.ok(symbols);
+  expect(symbols).not.to.be.undefined;
 
-	assert.strictEqual(symbols.length, symbolNames.length);
-	symbols.forEach((symbol, i) => {
-		assert.strictEqual(symbol.name, symbolNames[i]);
-	});
+  assert.strictEqual(symbols.length, symbolNames.length);
+  symbols.forEach((symbol, i) => {
+    assert.strictEqual(symbol.name, symbolNames[i]);
+  });
 }
