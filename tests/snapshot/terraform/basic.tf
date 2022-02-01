59987ae480a970b5955c94260abfa41fccb8577e
James Pogran <jpogran@outlook.com>
2022-02-01 08:57:09 -0500

    Terraform TextMate Test Infrastructure (#912)
    * Terraform TextMate Test Infrastructure
    
    This commit:
    
    - Adds testing infrastructure using vscode-tmgrammar-test to test the Terraform tmGrammar file. A minimal example file is used to demonstrate basic testing. Additional tests will be added as grammar is modified or changed.
    - Add tmGrammar schema to project to enable VS Code to provide intellisense and minimal validation of the tmGrammar files, whether they are json or yaml.
    - Updates the DEVELOPMENT readme and adds documentation on how to edit and test the Terraform tmGrammar
    - Automatically tests the grammar on every pull request

---
+++ tests/snapshot/terraform/basic.tf
@@ -0,0 +1,27 @@
+# line comment
+
+// line comment
+
+/*
+  Block comment
+*/
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.65"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "myTFResourceGroup"
+  location = "westus2"
+}
