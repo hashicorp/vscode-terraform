11e76a5a32898ca7887b413c0e553d57c9d35a52
James Pogran <jpogran@outlook.com>
2022-05-03 09:34:50 -0400

    Automatically refresh Terraform Providers View when providers change in open document (#1084)
    This commit automatically refreshes the Terraform Providers View when providers change in open document.
    
    This introduces a StaticFeature which registers a handler for the `client.refreshModuleProviders` request which terraform-ls can call to refresh the data shown in the Terraform Providers view. The terraform-ls server was modified to send `client.refreshModuleProviders` requests in https://github.com/hashicorp/terraform-ls/pull/902

---
+++ src/features/moduleProviders.ts
@@ -0,0 +1,37 @@
+import * as vscode from 'vscode';
+import { BaseLanguageClient, ClientCapabilities, ServerCapabilities, StaticFeature } from 'vscode-languageclient';
+import { ModuleProvidersDataProvider } from '../providers/moduleProviders';
+import { ExperimentalClientCapabilities } from './types';
+
+export const CLIENT_MODULE_PROVIDERS_CMD_ID = 'client.refreshModuleProviders';
+
+export class ModuleProvidersFeature implements StaticFeature {
+  private disposables: vscode.Disposable[] = [];
+
+  constructor(private client: BaseLanguageClient, private view: ModuleProvidersDataProvider) {}
+
+  public fillClientCapabilities(capabilities: ClientCapabilities & ExperimentalClientCapabilities): void {
+    if (!capabilities['experimental']) {
+      capabilities['experimental'] = {};
+    }
+    capabilities['experimental']['refereshModuleProvidersCommandId'] = CLIENT_MODULE_PROVIDERS_CMD_ID;
+  }
+
+  public async initialize(capabilities: ServerCapabilities): Promise<void> {
+    if (!capabilities.experimental?.refreshModuleProviders) {
+      console.log("Server doesn't support client.refreshModuleProviders");
+      return;
+    }
+
+    await this.client.onReady();
+
+    const d = this.client.onRequest(CLIENT_MODULE_PROVIDERS_CMD_ID, () => {
+      this.view?.refresh();
+    });
+    this.disposables.push(d);
+  }
+
+  public dispose(): void {
+    this.disposables.forEach((d: vscode.Disposable) => d.dispose());
+  }
+}
