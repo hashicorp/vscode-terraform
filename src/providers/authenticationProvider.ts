8905268753ca6e046704c3ef7a9cd8d2a776c365
James Pogran <jpogran@outlook.com>
2023-05-05 06:20:09 -0400

    Implement Terraform Cloud Authentication Provider (#1411)
    * Terraform Cloud Host Setting
    
    Adds a setting for terraform.cloud.hostname and sets app.terraform.io as default value.
    
    Closes #1398
    
    * Implement Terraform Cloud Authentication Provider
    
    This implements the `vscode.AuthenticationProvider` interface and creates a TFC specific provider. This handles listing existing sessions, creating and removing sessions.
    
    Storing sessions/tokens will be improved by https://github.com/hashicorp/vscode-terraform/issues/1397. Currently it only securely stores the token. Future improvement can be done to prevent looking up user data every time the session is reloaded.
    
    The TFC instance to target is defaulted to `app.terraform.io` with a configuration setting to override. This was added in https://github.com/hashicorp/vscode-terraform/issues/1398. Future work should discover whether detecting the TFC instance from the configuration available in the current working space is desirable.
    
    One of the primary ways a user will trigger authentication will be through the VS Code Command palette commands, so https://github.com/hashicorp/vscode-terraform/issues/1399 is added here to finish out the login workflow.
    
    This currently only supports one logged in user at a time. In the future, this may be extended to support multiple user tokens.
    
    Closes #1396
    
    * Remove type-cast for promise
    
    * Update src/providers/authenticationProvider.ts
    
    Co-authored-by: Daniel Banck <dbanck@users.noreply.github.com>
    
    * Update package.json
    
    Co-authored-by: Daniel Banck <dbanck@users.noreply.github.com>
    
    * Update lockfile
    
    ---------
    
    Co-authored-by: Daniel Banck <daniel@dbanck.de>
    Co-authored-by: Daniel Banck <dbanck@users.noreply.github.com>

---
+++ src/providers/authenticationProvider.ts
@@ -0,0 +1,221 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import * as vscode from 'vscode';
+import { getUser } from './tfc';
+
+class TerraformCloudSession implements vscode.AuthenticationSession {
+  // This id isn't used for anything yet, so we set it to a constant
+  readonly id = TerraformCloudAuthenticationProvider.providerID;
+
+  // In the future, we may use the UAT permissions as scopes
+  // but right now have no use fior them, so we have an empty array here.
+  readonly scopes = [];
+
+  /**
+   *
+   * @param accessToken The personal access token to use for authentication
+   * @param account The user account for the specified token
+   */
+  constructor(public readonly accessToken: string, public account: vscode.AuthenticationSessionAccountInformation) {}
+}
+
+export class TerraformCloudAuthenticationProvider implements vscode.AuthenticationProvider, vscode.Disposable {
+  static providerLabel = 'HashiCorp Terraform Cloud';
+  static providerID = 'HashiCorpTerraformCloud';
+  private static secretKey = 'HashiCorpTerraformCloud';
+  private logger: vscode.LogOutputChannel;
+
+  // this property is used to determine if the token has been changed in another window of VS Code.
+  private currentToken: string | undefined;
+  private initializedDisposable: vscode.Disposable | undefined;
+
+  private _onDidChangeSessions =
+    new vscode.EventEmitter<vscode.AuthenticationProviderAuthenticationSessionsChangeEvent>();
+
+  constructor(private readonly secretStorage: vscode.SecretStorage, private readonly ctx: vscode.ExtensionContext) {
+    this.logger = vscode.window.createOutputChannel('HashiCorp Authentication', { log: true });
+    ctx.subscriptions.push(
+      vscode.commands.registerCommand('terraform.cloud.login', async () => {
+        const session = await vscode.authentication.getSession(TerraformCloudAuthenticationProvider.providerID, [], {
+          createIfNone: true,
+        });
+        vscode.window.showInformationMessage(`Hello ${session.account.label}`);
+      }),
+    );
+  }
+
+  get onDidChangeSessions(): vscode.Event<vscode.AuthenticationProviderAuthenticationSessionsChangeEvent> {
+    return this._onDidChangeSessions.event;
+  }
+
+  // This function is called first when `vscode.authentication.getSessions` is called.
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async getSessions(scopes?: string[] | undefined): Promise<readonly vscode.AuthenticationSession[]> {
+    this.ensureInitialized();
+
+    this.logger.info('Reading sessions from storage...');
+    const token = await this.cacheTokenFromStorage();
+
+    if (token === undefined) {
+      this.logger.info('No stored sessions!');
+      return [];
+    }
+
+    this.logger.info('Got stored sessions!');
+
+    try {
+      // temporary TFC access method here
+      // will be replaced by TFC REST API wrapper
+      const user = await getUser(token);
+      this.logger.info('Got user info');
+
+      return token
+        ? [
+            new TerraformCloudSession(token, {
+              label: user.data.attributes.username,
+              id: user.data.attributes.email,
+            }),
+          ]
+        : [];
+    } catch (error) {
+      if (error instanceof Error) {
+        vscode.window.showErrorMessage(error.message);
+      } else if (typeof error === 'string') {
+        vscode.window.showErrorMessage(error);
+      }
+
+      // TODO: Handle 401 auth errors here
+      return [];
+    }
+  }
+
+  // This function is called after `this.getSessions` is called and only when:
+  // - `this.getSessions` returns nothing but `createIfNone` was set to `true` in `vscode.authentication.getSessions`
+  // - `vscode.authentication.getSessions` was called with `forceNewSession: true`
+  // - The end user initiates the "silent" auth flow via the Accounts menu
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async createSession(_scopes: readonly string[]): Promise<vscode.AuthenticationSession> {
+    this.ensureInitialized();
+
+    // Prompt for the UAT.
+    const token = await vscode.window.showInputBox({
+      ignoreFocusOut: true,
+      placeHolder: 'User access token',
+      prompt: 'Enter an HashiCorp Terraform User Access Token (UAT).',
+      password: true,
+    });
+
+    // Note: this example doesn't do any validation of the token beyond making sure it's not empty.
+    if (!token) {
+      this.logger.error('User did not provide a UAT');
+      throw new Error('UAT is required');
+    }
+
+    try {
+      // temporary TFC access method here
+      // will be replaced by TFC REST API wrapper
+      const user = await getUser(token);
+
+      // Don't set `currentToken` here, since we want to fire the proper events in the `checkForUpdates` call
+      await this.secretStorage.store(TerraformCloudAuthenticationProvider.secretKey, token);
+      this.logger.info('Successfully logged in to HashiCorp Terraform');
+
+      const session = new TerraformCloudSession(token, {
+        label: user.data.attributes.username,
+        id: user.data.attributes.email,
+      });
+
+      this._onDidChangeSessions.fire({ added: [session], removed: [], changed: [] });
+      // label is what is display in the UI
+      return session;
+    } catch (error) {
+      if (error instanceof Error) {
+        vscode.window.showErrorMessage(error.message);
+        this.logger.error(error.message);
+      } else if (typeof error === 'string') {
+        vscode.window.showErrorMessage(error);
+        this.logger.error(error);
+      }
+
+      // TODO: Handle 401 auth errors here
+      throw error;
+    }
+  }
+
+  // This function is called when the end user signs out of the account.
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async removeSession(_sessionId: string): Promise<void> {
+    this.logger.info('Detecting current logged in session');
+    const session = (await this.getSessions())[0];
+
+    this.logger.info('Removing current session');
+    await this.secretStorage.delete(TerraformCloudAuthenticationProvider.secretKey);
+
+    this._onDidChangeSessions.fire({ added: [], removed: [session], changed: [] });
+  }
+
+  private async ensureInitialized(): Promise<void> {
+    if (this.initializedDisposable !== undefined) {
+      return;
+    }
+
+    await this.cacheTokenFromStorage();
+
+    this.initializedDisposable = vscode.Disposable.from(
+      // This onDidChange event happens when the secret storage changes in _any window_ since
+      // secrets are shared across all open windows.
+      this.secretStorage.onDidChange((e) => {
+        if (e.key === TerraformCloudAuthenticationProvider.secretKey) {
+          this.logger.info('Authentication secret storage change');
+          void this.checkForUpdates();
+        }
+      }),
+      // This fires when the user initiates a "silent" auth flow via the Accounts menu.
+      vscode.authentication.onDidChangeSessions((e) => {
+        if (e.provider.id === TerraformCloudAuthenticationProvider.providerID) {
+          this.logger.info('Authentication provider change');
+          void this.checkForUpdates();
+        }
+      }),
+    );
+  }
+
+  // This is a crucial function that handles whether or not the token has changed in
+  // a different window of VS Code and sends the necessary event if it has.
+  private async checkForUpdates(): Promise<void> {
+    const added: vscode.AuthenticationSession[] = [];
+    const removed: vscode.AuthenticationSession[] = [];
+    const changed: vscode.AuthenticationSession[] = [];
+
+    const previousToken = this.currentToken;
+    const session = (await this.getSessions())[0];
+
+    if (session?.accessToken && !previousToken) {
+      this.logger.info('Session added');
+      added.push(session);
+    } else if (!session?.accessToken && previousToken) {
+      this.logger.info('Session removed');
+      removed.push(session);
+    } else if (session?.accessToken !== previousToken) {
+      this.logger.info('Session changed');
+      changed.push(session);
+    } else {
+      return;
+    }
+
+    await this.cacheTokenFromStorage();
+    this._onDidChangeSessions.fire({ added: added, removed: removed, changed: changed });
+  }
+
+  private async cacheTokenFromStorage() {
+    this.currentToken = await this.secretStorage.get(TerraformCloudAuthenticationProvider.secretKey);
+    return this.currentToken;
+  }
+
+  dispose() {
+    this.initializedDisposable?.dispose();
+  }
+}
