b5e11c029ed439a479b7274983f02a11326efc70
James Pogran <jpogran@outlook.com>
2023-05-05 06:20:09 -0400

    Implement Terraform Cloud Authentication Provider (#1411)
    * Terraform Cloud Host Setting
    
    Adds a setting for terraform.cloud.hostname and sets app.terraform.io as default value.
    
    Closes #1398
    
    * Implement Terraform Cloud Authentication Provider
    
    This implements the `vscode.AuthenticationProvider` interface and creates a TFC specific provider. This handles listing existing sessions, creating and removing sessions.
    
    Storing sessions/tokens will be improved by https://github.com/hashicorp/vscode-terraform/issues/1397. Currently it only securely stores the token. Future improvement can be done to prevent looking up user data every time the session is reloaded.
    
    The TFC instance to target is defaulted to `app.terraform.io` with a configuration setting to override. This was added in https://github.com/hashicorp/vscode-terraform/issues/1398. Future work should discover whether detecting the TFC instance from the configuration available in the current working space is desirable.
    
    One of the primary ways a user will trigger authentication will be through the VS Code Command palette commands, so https://github.com/hashicorp/vscode-terraform/issues/1399 is added here to finish out the login workflow.
    
    This currently only supports one logged in user at a time. In the future, this may be extended to support multiple user tokens.
    
    Closes #1396
    
    * Remove type-cast for promise
    
    * Update src/providers/authenticationProvider.ts
    
    Co-authored-by: Daniel Banck <dbanck@users.noreply.github.com>
    
    * Update package.json
    
    Co-authored-by: Daniel Banck <dbanck@users.noreply.github.com>
    
    * Update lockfile
    
    ---------
    
    Co-authored-by: Daniel Banck <daniel@dbanck.de>
    Co-authored-by: Daniel Banck <dbanck@users.noreply.github.com>

--- src/extension.ts
+++ src/extension.ts
@@ -32,6 +32,7 @@ import { getInitializationOptions, migrateLegacySettings } from './settings';
 import { TerraformLSCommands } from './commands/terraformls';
 import { TerraformCommands } from './commands/terraform';
 import { TerraformVersionFeature } from './features/terraformVersion';
+import { TerraformCloudAuthenticationProvider } from './providers/authenticationProvider';
 
 const id = 'terraform';
 const brand = `HashiCorp Terraform`;
@@ -55,6 +56,15 @@ export async function activate(context: vscode.ExtensionContext): Promise<void> 
 
   // always register commands needed to control terraform-ls
   context.subscriptions.push(new TerraformLSCommands());
+
+  context.subscriptions.push(
+    vscode.authentication.registerAuthenticationProvider(
+      TerraformCloudAuthenticationProvider.providerID,
+      TerraformCloudAuthenticationProvider.providerLabel,
+      new TerraformCloudAuthenticationProvider(context.secrets, context),
+      { supportsMultipleAccounts: false },
+    ),
+  );
 
   if (config('terraform').get<boolean>('languageServer.enable') === false) {
     reporter.sendTelemetryEvent('disabledTerraformLS');
