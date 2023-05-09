e119009e44d5f915f7b7f09dc79744d1d64bbd3c
Radek Simko <radek.simko@gmail.com>
2023-05-09 16:56:08 +0100

    Implement TFC REST API client (#1416)
    * Add allowSyntheticDefaultImports to TS options
    
    We have to enable this to get proper types from Zodios.
    
    This flag does not affect the JavaScript emitted by TypeScript,
    it only for the type checking.
    
    * eslint: Disable @typescript-eslint/naming-convention
    
    * Introduce terraformCloud API client
    
    * Add endpoint descriptions
    
    * Add projects endpoint
    
    * Add pagination to endpoints
    
    * Enable filtering of workspaces
    
    * Split endpoints into separate files
    
    * Keep earlyApiClient as earlyApiClient to avoid confusion
    
    * terraformCloud: add missing enums for run & workspace
    
    * Address/update remaining TODOs
    
    * Add a test mock server using msw
    
    ---------
    
    Co-authored-by: Daniel Banck <daniel@dbanck.de>

---
+++ src/test/integration/mocks/server.ts
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import { ZodiosPathsByMethod, ZodiosResponseByPath } from '@zodios/core/lib/zodios.types';
+import { ResponseResolver, rest, RestContext, RestRequest } from 'msw';
+import { setupServer } from 'msw/node';
+import { apiClient, baseUrl } from '../../../terraformCloud';
+
+type Api = typeof apiClient.api;
+
+export function mockGet<Path extends ZodiosPathsByMethod<Api, 'get'>>(
+  path: Path,
+  resolver: ResponseResolver<RestRequest, RestContext, Awaited<ZodiosResponseByPath<Api, 'get', Path>>>,
+) {
+  return rest.get(`${baseUrl}${path}`, resolver);
+}
+
+const handlers = [
+  mockGet('/account/details', (_, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({
+        data: {
+          id: 'user-1',
+          type: 'user',
+          attributes: {
+            username: 'user',
+            email: 'user@example.com',
+          },
+        },
+      }),
+    );
+  }),
+];
+
+// This configures a request mocking server with the given request handlers.
+export const server = setupServer(...handlers);
