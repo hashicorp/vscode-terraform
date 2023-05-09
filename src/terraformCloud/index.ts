0944bba7fb3a974e4b49ec9d8f8ab85176286980
Radek Simko <radek.simko@gmail.com>
2023-05-09 16:12:33 +0100

    Address/update remaining TODOs

--- src/terraformCloud/index.ts
+++ src/terraformCloud/index.ts
@@ -13,36 +13,17 @@ import { projectEndpoints } from './project';
 import { runEndpoints } from './run';
 import { workspaceEndpoints } from './workspace';
 
-const defaultHostname = 'app.terraform.io';
-const basePath = '/api/v2';
+// TODO: Replace with production URL before going live
+export const baseUrl = 'https://app.staging.terraform.io/api/v2';
 
 const jsonHeader = pluginHeader('Content-Type', async () => 'application/vnd.api+json');
 
-// TODO: Consider passing it as a dependency instead of global access?
+// TODO: Consider passing it as a dependency instead of global access to make testing easier
 const extVersion = vscode.extensions.getExtension('hashicorp.terraform')?.packageJSON.version;
 const userAgentHeader = pluginHeader(
   'User-Agent',
   async () => `VSCode/${vscode.version} hashicorp.terraform/${extVersion}`,
 );
-
-function getBaseURL(): string {
-  // TODO: decide where to keep default value?
-  const baseUrl = new URL(basePath, `https://${defaultHostname}`);
-  const hostname = vscode.workspace.getConfiguration('terraform.cloud').get<string>('hostname');
-  if (hostname) {
-    baseUrl.hostname = hostname;
-  }
-  return baseUrl.toString();
-}
-
-function pluginBaseURL(baseURLFn: () => Promise<string>): ZodiosPlugin {
-  return {
-    request: async (_, config) => {
-      const baseURL = await baseURLFn();
-      return { ...config, baseURL };
-    },
-  };
-}
 
 function pluginLogger(): ZodiosPlugin {
   return {
@@ -53,17 +34,12 @@ function pluginLogger(): ZodiosPlugin {
   };
 }
 
-export const earlyApiClient = new Zodios(getBaseURL(), accountEndpoints);
+export const earlyApiClient = new Zodios(baseUrl, accountEndpoints);
 earlyApiClient.use(jsonHeader);
 earlyApiClient.use(userAgentHeader);
-earlyApiClient.use(
-  pluginBaseURL(async () => {
-    return getBaseURL();
-  }),
-);
 earlyApiClient.use(pluginLogger());
 
-export const apiClient = new Zodios(getBaseURL(), [
+export const apiClient = new Zodios(baseUrl, [
   ...accountEndpoints,
   ...organizationEndpoints,
   ...projectEndpoints,
@@ -75,7 +51,7 @@ apiClient.use(userAgentHeader);
 apiClient.use(
   pluginToken({
     getToken: async () => {
-      // TODO: Consider passing it as a dependency instead of global access?
+      // TODO: Consider passing it as a dependency instead of global access to make testing easier
       const session = await vscode.authentication.getSession(TerraformCloudAuthenticationProvider.providerID, [], {
         createIfNone: true,
       });
