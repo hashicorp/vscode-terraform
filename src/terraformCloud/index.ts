b5c23b7eff616d9a975665be9ffaf1d10c730bf4
James Pogran <jpogran@outlook.com>
2023-06-15 11:58:01 -0400

    Change to production URL (#1493)
    * Change to production URL
    
    ---------
    
    Co-authored-by: Radek Simko <radek.simko@gmail.com>

--- src/terraformCloud/index.ts
+++ src/terraformCloud/index.ts
@@ -13,8 +13,7 @@ import { projectEndpoints } from './project';
 import { runEndpoints } from './run';
 import { workspaceEndpoints } from './workspace';
 
-// TODO: replace with production URL
-export const TerraformCloudHost = 'app.staging.terraform.io';
+export const TerraformCloudHost = 'app.terraform.io';
 
 export const TerraformCloudAPIUrl = `https://${TerraformCloudHost}/api/v2`;
 export const TerraformCloudWebUrl = `https://${TerraformCloudHost}/app`;
