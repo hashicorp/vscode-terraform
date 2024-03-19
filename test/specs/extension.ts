5a122330f03f4883890548c5ff5e48e84d4d5f76
James Pogran <jpogran@outlook.com>
2024-03-19 12:01:22 -0400

    Webdriver.io End to End Testing (#1691)
    Introduces new tests using webdriver.io as an integration testing framework for vscode extensions.
    
    This aims to supplement, not replace the existing testing inside terraform-ls. There are some things that are difficult to test with the current testing framework, such as the UI, and the interaction between the extension and the language server.
    
    This is a first step towards a more comprehensive testing strategy for the extension. As such, it is not expected to be perfect, and will likely need to be iterated on. Also, we likely will move the location, but want to get the initial setup in place.
    
    Co-authored-by: Daniel Banck <daniel@dbanck.de>

---
+++ test/specs/extension.ts
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import { browser, expect } from '@wdio/globals';
+
+describe('VS Code Extension Testing', () => {
+  it('should be able to load VSCode', async () => {
+    const workbench = await browser.getWorkbench();
+    expect(await workbench.getTitleBar().getTitle()).toContain('[Extension Development Host]');
+  });
+
+  it('should load and install our VSCode Extension', async () => {
+    const extensions = await browser.executeWorkbench((vscodeApi) => {
+      return vscodeApi.extensions.all;
+    });
+    expect(extensions.some((extension) => extension.id === 'hashicorp.terraform')).toBe(true);
+  });
+
+  it('should show all activity bar items', async () => {
+    const workbench = await browser.getWorkbench();
+    const viewControls = await workbench.getActivityBar().getViewControls();
+    expect(await Promise.all(viewControls.map((vc) => vc.getTitle()))).toEqual([
+      'Explorer',
+      'Search',
+      'Source Control',
+      'Run and Debug',
+      'Extensions',
+      'HashiCorp Terraform',
+      'HashiCorp Terraform Cloud',
+    ]);
+  });
+
+  // this does not appear to work in CI
+  // it('should start the ls', async () => {
+  //   const workbench = await browser.getWorkbench();
+  //   await workbench.executeCommand('workbench.panel.output.focus');
+
+  //   const bottomBar = workbench.getBottomBar();
+  //   await bottomBar.maximize();
+
+  //   const outputView = await bottomBar.openOutputView();
+  //   await outputView.wait();
+  //   await outputView.selectChannel('HashiCorp Terraform');
+  //   const output = await outputView.getText();
+
+  //   expect(output.some((element) => element.toLowerCase().includes('dispatching next job'.toLowerCase()))).toBeTruthy();
+  // });
+});
