d2490880ea66f605438c38c67021111d603dc87d
Ansgar Mertens <ansgar@hashicorp.com>
2024-04-24 14:47:43 +0200

    feat: add test for completing from bundled schema

--- src/test/integration/init/workspace/main.tf
+++ src/test/integration/init/workspace/main.tf
@@ -2,11 +2,15 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "5.46.0"
+      version = "5.45.0"
     }
   }
 }
 
 provider "aws" {
   # Configuration options
+}
+
+resource "aws_eip_domain_name" "name" {
+
 }
