7fbd0f266b82ba5655463977d26e05796fca8070
Ansgar Mertens <ansgar@hashicorp.com>
2024-04-26 17:35:11 +0200

    feat: add test for completing module from git
    todo: this currently breaks the downloaded provider testcase and I don't know why. It's reproducible. A longer waiting time does not fix it. It can be fixed by removing git_module.tf. It works if init had been run before the test (either in terminal or still there from a previous run). So this hints at the LS doing weird things and not updating for unknown reasons

--- src/test/integration/init/init.test.ts
+++ src/test/integration/init/init.test.ts
@@ -75,4 +75,71 @@ suite('init', () => {
       });
     });
   });
+
+  suite('with module schema from git', function suite() {
+    const docUri = getDocUri('git_module.tf');
+
+    this.beforeAll(async () => {
+      await open(docUri);
+      await activateExtension();
+
+      // run terraform init command to download provider schema
+      await vscode.commands.executeCommand('terraform.initCurrent');
+      // wait for schema to be loaded
+      await sleep(5_000);
+    });
+
+    this.afterAll(async () => {
+      // remove .terraform directory
+      const dotTerraform = getDocUri('.terraform');
+      await vscode.workspace.fs.delete(dotTerraform, { recursive: true });
+      // remove .terraform.lock.hcl
+      const lockfile = getDocUri('.terraform.lock.hcl');
+      await vscode.workspace.fs.delete(lockfile);
+    });
+
+    teardown(async () => {
+      await vscode.commands.executeCommand('workbench.action.closeAllEditors');
+    });
+
+    test('language is registered', async () => {
+      const doc = await vscode.workspace.openTextDocument(docUri);
+      assert.equal(doc.languageId, 'terraform', 'document language should be `terraform`');
+    });
+
+    test('completes module from downloaded schema', async () => {
+      const expected = [
+        new vscode.CompletionItem('allow_users_to_change_password', vscode.CompletionItemKind.Property),
+        new vscode.CompletionItem('count', vscode.CompletionItemKind.Property),
+        new vscode.CompletionItem('create_account_password_policy', vscode.CompletionItemKind.Property),
+        new vscode.CompletionItem('depends_on', vscode.CompletionItemKind.Property),
+        new vscode.CompletionItem('for_each', vscode.CompletionItemKind.Property),
+        new vscode.CompletionItem('get_caller_identity', vscode.CompletionItemKind.Property),
+        new vscode.CompletionItem('hard_expiry', vscode.CompletionItemKind.Property),
+        new vscode.CompletionItem('max_password_age', vscode.CompletionItemKind.Property),
+        new vscode.CompletionItem('minimum_password_length', vscode.CompletionItemKind.Property),
+        new vscode.CompletionItem('password_reuse_prevention', vscode.CompletionItemKind.Property),
+        new vscode.CompletionItem('providers', vscode.CompletionItemKind.Property),
+        new vscode.CompletionItem('require_lowercase_characters', vscode.CompletionItemKind.Property),
+        new vscode.CompletionItem('require_numbers', vscode.CompletionItemKind.Property),
+        new vscode.CompletionItem('require_symbols', vscode.CompletionItemKind.Property),
+        new vscode.CompletionItem('require_uppercase_characters', vscode.CompletionItemKind.Property),
+        new vscode.CompletionItem('version', vscode.CompletionItemKind.Property),
+        // snippets
+        new vscode.CompletionItem({ label: 'fore', description: 'For Each' }, vscode.CompletionItemKind.Snippet),
+        new vscode.CompletionItem({ label: 'module', description: 'Module' }, vscode.CompletionItemKind.Snippet),
+        new vscode.CompletionItem({ label: 'output', description: 'Output' }, vscode.CompletionItemKind.Snippet),
+        new vscode.CompletionItem(
+          { label: 'provisioner', description: 'Provisioner' },
+          vscode.CompletionItemKind.Snippet,
+        ),
+        new vscode.CompletionItem({ label: 'vare', description: 'Empty variable' }, vscode.CompletionItemKind.Snippet),
+        new vscode.CompletionItem({ label: 'varm', description: 'Map variable' }, vscode.CompletionItemKind.Snippet),
+      ];
+
+      await testCompletion(docUri, new vscode.Position(2, 5), {
+        items: expected,
+      });
+    });
+  });
 });
