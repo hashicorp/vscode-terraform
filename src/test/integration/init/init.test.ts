d5e5fb62f0b89b43f7bd7e3e400a9499bbe164f6
Ansgar Mertens <ansgar@hashicorp.com>
2024-04-26 15:16:41 +0200

    feat: add test for completing from downloaded schema

--- src/test/integration/init/init.test.ts
+++ src/test/integration/init/init.test.ts
@@ -5,7 +5,7 @@
 
 import * as vscode from 'vscode';
 import { assert } from 'chai';
-import { activateExtension, getDocUri, open, testCompletion } from '../../helper';
+import { activateExtension, getDocUri, open, testCompletion, sleep } from '../../helper';
 
 suite('init', () => {
   suite('with bundled provider schema', function suite() {
@@ -29,7 +29,48 @@ suite('init', () => {
       // aws_eip_domain_name was added in provider version 5.46.0
       const expected = [new vscode.CompletionItem('aws_eip_domain_name', vscode.CompletionItemKind.Field)];
 
-      await testCompletion(docUri, new vscode.Position(13, 26), {
+      await testCompletion(docUri, new vscode.Position(13, 25), {
+        items: expected,
+      });
+    });
+  });
+
+  suite('with provider schema from init', function suite() {
+    const docUri = getDocUri('main.tf');
+
+    this.beforeAll(async () => {
+      await open(docUri);
+      await activateExtension();
+
+      // run terraform init command to download provider schema
+      await vscode.commands.executeCommand('terraform.initCurrent');
+      // wait for schema to be loaded
+      await sleep(5_000);
+    });
+
+    this.afterAll(async () => {
+      // remove .terraform directory
+      const dotTerraform = getDocUri('.terraform');
+      await vscode.workspace.fs.delete(dotTerraform, { recursive: true });
+      // remove .terraform.lock.hcl
+      const lockfile = getDocUri('.terraform.lock.hcl');
+      await vscode.workspace.fs.delete(lockfile);
+    });
+
+    teardown(async () => {
+      await vscode.commands.executeCommand('workbench.action.closeAllEditors');
+    });
+
+    test('language is registered', async () => {
+      const doc = await vscode.workspace.openTextDocument(docUri);
+      assert.equal(doc.languageId, 'terraform', 'document language should be `terraform`');
+    });
+
+    test('completes resource not available in downloaded schema', async () => {
+      // aws_eip_domain_name was added in provider version 5.46.0 but we initialized with 5.45.0
+      const expected: vscode.CompletionItem[] = [];
+
+      await testCompletion(docUri, new vscode.Position(13, 25), {
         items: expected,
       });
     });
