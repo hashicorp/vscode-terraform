5c3da5dc411d151e287a2bea27f0832f00f98fca
James Pogran <jpogran@outlook.com>
2024-04-29 14:41:51 -0400

    maybe

--- .vscode-test.mjs
+++ .vscode-test.mjs
@@ -4,6 +4,8 @@
  */
 
 import { defineConfig } from '@vscode/test-cli';
+import { mkdtempSync } from 'fs';
+import * as os from 'os';
 import fs from 'fs';
 import path from 'path';
 
@@ -11,27 +13,33 @@ import path from 'path';
 const BASE_SRC_PATH = './src/test/integration';
 const BASE_OUT_PATH = './out/test/integration';
 
-const testSuiteFolderNames = fs.readdirSync(BASE_SRC_PATH, { withFileTypes: true })
-  .filter(entry => entry.isDirectory()) // only directories ...
-  .filter(entry => fs.existsSync(path.join(BASE_SRC_PATH, entry.name, "workspace"))) // ... that contain a workspace folder are valid
-  .map(entry => entry.name);
+const testSuiteFolderNames = fs
+  .readdirSync(BASE_SRC_PATH, { withFileTypes: true })
+  .filter((entry) => entry.isDirectory()) // only directories ...
+  .filter((entry) => fs.existsSync(path.join(BASE_SRC_PATH, entry.name, 'workspace'))) // ... that contain a workspace folder are valid
+  .map((entry) => entry.name);
 
-const configs = testSuiteFolderNames.map((folderName) => ({
-  version: process.env['VSCODE_VERSION'] ?? 'stable',
-  workspaceFolder: process.env['VSCODE_WORKSPACE_FOLDER'] ?? path.join(BASE_SRC_PATH, folderName, 'workspace'),
-  launchArgs: ['--profile-temp', '--disable-extensions', '--disable-workspace-trust'],
-  files: `${BASE_OUT_PATH}/${folderName}/*.test.js`,
-  env: {
-    DISPLAY: ':99.0',
-  },
-  mocha: {
-    ui: 'tdd',
-    color: true,
-    timeout: 100000,
-    // require: ['./out/test/mockSetup.js'], // mocks are shared for all test suites
-  },
-}));
+const configs = testSuiteFolderNames.map((folderName) => mkConfig(folderName));
 
 const config = defineConfig(configs);
 
 export default config;
+
+function mkConfig(folderName) {
+  const tmpdir = mkdtempSync(`${os.tmpdir()}/vsc-ada-test-`);
+  return {
+    label: `tf extension testsuite: ${folderName}`,
+    version: process.env['VSCODE_VERSION'] ?? 'stable',
+    workspaceFolder: process.env['VSCODE_WORKSPACE_FOLDER'] ?? path.join(BASE_SRC_PATH, folderName, 'workspace'),
+    launchArgs: [`--user-data-dir=${tmpdir}`, '--profile-temp', '--disable-extensions', '--disable-workspace-trust'],
+    files: `${BASE_OUT_PATH}/${folderName}/*.test.js`,
+    env: {
+      DISPLAY: ':99.0',
+    },
+    mocha: {
+      ui: 'tdd',
+      color: true,
+      timeout: 100000,
+    },
+  };
+}
